ea184c942c45e32c07365b92bcf6e0c8b833e0cf
Roman S. Borschel <roman.borschel@googlemail.com>
2010-05-23 01:01:00 +0200

    Updates for dependency management.

--- tests/Doctrine/Tests/TestInit.php
+++ tests/Doctrine/Tests/TestInit.php
@@ -8,10 +8,7 @@ error_reporting(E_ALL | E_STRICT);
 
 require_once 'PHPUnit/Framework.php';
 require_once 'PHPUnit/TextUI/TestRunner.php';
-require_once __DIR__ . '/../../../lib/vendor/doctrine-common/lib/Doctrine/Common/ClassLoader.php';
-
-$classLoader = new \Doctrine\Common\ClassLoader('Doctrine\Common', __DIR__ . "/../../../lib/vendor/doctrine-common/lib");
-$classLoader->register();
+require_once __DIR__ . '/../../../lib/Doctrine/Common/ClassLoader.php';
 
 $classLoader = new \Doctrine\Common\ClassLoader('Doctrine');
 $classLoader->register();
