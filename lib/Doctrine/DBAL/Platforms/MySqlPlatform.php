doctrine/dbal

commit e8fa2e1f038f385723a2fb500845aa07daa0e044

DDC-625 - Fix bug in DBAL MySQL Platform detecting foreign keys

lib/Doctrine/DBAL/Platforms/MySqlPlatform.php
@@ -124,12 +124,12 @@ class MySqlPlatform extends AbstractPlatform
         $sql = "SELECT DISTINCT k.`CONSTRAINT_NAME`, k.`COLUMN_NAME`, k.`REFERENCED_TABLE_NAME`, ".
                "k.`REFERENCED_COLUMN_NAME` /*!50116 , c.update_rule, c.delete_rule */ ".
                "FROM information_schema.key_column_usage k /*!50116 ".
-               "INNER JOIN information_schema.referential_constraints c ON k.`CONSTRAINT_NAME` = c.constraint_name AND ".
+               "INNER JOIN information_schema.referential_constraints c ON ".
                "  c.constraint_name = k.constraint_name AND ".
                "  c.table_name = '$table' */ WHERE k.table_name = '$table'";
 
         if ( ! is_null($database)) {
-            $sql .= " AND table_schema = '$database'";
+            $sql .= " AND c.table_schema = '$database' AND k.constraint_schema = '" . $database  . "'";
         }
 
         $sql .= " AND `REFERENCED_COLUMN_NAME` is not NULL";

commit 669aed8f522d7af69af39788207332c83bcbea64

Fixed bug introduced in e8fa2e1f038f385723a2, fixing DDC-625. Fixed bug in testsuite

lib/Doctrine/DBAL/Platforms/MySqlPlatform.php
@@ -128,8 +128,8 @@ class MySqlPlatform extends AbstractPlatform
                "  c.constraint_name = k.constraint_name AND ".
                "  c.table_name = '$table' */ WHERE k.table_name = '$table'";
 
-        if ( ! is_null($database)) {
-            $sql .= " AND c.table_schema = '$database' AND k.constraint_schema = '" . $database  . "'";
+        if ($database) {
+            $sql .= " AND k.table_schema = '$database' AND c.constraint_schema = '$database'";
         }
 
         $sql .= " AND `REFERENCED_COLUMN_NAME` is not NULL";

tests/Doctrine/Tests/DBAL/Functional/AllTests.php
@@ -25,15 +25,15 @@ class AllTests
         $conn= TestUtil::getConnection();
         $sm = $conn->getSchemaManager();
 
-        if ($sm instanceof Doctrine\DBAL\Schema\SqliteSchemaManager) {
+        if ($sm instanceof \Doctrine\DBAL\Schema\SqliteSchemaManager) {
             $suite->addTestSuite('Doctrine\Tests\DBAL\Functional\Schema\SqliteSchemaManagerTest');
-        } else if ($sm instanceof Doctrine\DBAL\Schema\MySqlSchemaManager) {
+        } else if ($sm instanceof \Doctrine\DBAL\Schema\MySqlSchemaManager) {
             $suite->addTestSuite('Doctrine\Tests\DBAL\Functional\Schema\MySqlSchemaManagerTest');
-        } else if ($sm instanceof Doctrine\DBAL\Schema\PostgreSqlSchemaManager) {
+        } else if ($sm instanceof \Doctrine\DBAL\Schema\PostgreSqlSchemaManager) {
             $suite->addTestSuite('Doctrine\Tests\DBAL\Functional\Schema\PostgreSqlSchemaManagerTest');
-        } else if ($sm instanceof Doctrine\DBAL\Schema\OracleSchemaManager) {
+        } else if ($sm instanceof \Doctrine\DBAL\Schema\OracleSchemaManager) {
             $suite->addTestSuite('Doctrine\Tests\DBAL\Functional\Schema\OracleSchemaManagerTest');
-        } else if ($sm instanceof Doctrine\DBAL\Schema\DB2SchemaManager) {
+        } else if ($sm instanceof \Doctrine\DBAL\Schema\DB2SchemaManager) {
             $suite->addTestSuite('Doctrine\Tests\DBAL\Functional\Schema\Db2SchemaManagerTest');
         }
         $suite->addTestSuite('Doctrine\Tests\DBAL\Functional\ConnectionTest');