a1e4c86b565e0161204596745b6c61dd900f59c2
Benjamin Eberlei <kontakt@beberlei.de>
2010-07-24 17:42:25 +0200

    DBAL-33 - Added flexible DateTime type that can be used as replacement when variable date/time parts are returned from the database, for example with PostgreSQL TIMESTAMP(n) and TIME(n) types

---
+++ tests/Doctrine/Tests/DBAL/Types/VarDateTimeTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Doctrine\Tests\DBAL\Types;
+
+use Doctrine\DBAL\Types\Type;
+use Doctrine\Tests\DBAL\Mocks;
+
+require_once __DIR__ . '/../../TestInit.php';
+
+class VarDateTimeTest extends \Doctrine\Tests\DbalTestCase
+{
+    protected
+        $_platform,
+        $_type;
+
+    protected function setUp()
+    {
+        $this->_platform = new \Doctrine\Tests\DBAL\Mocks\MockPlatform();
+        if (!Type::hasType('vardatetime')) {
+            Type::addType('vardatetime', 'Doctrine\DBAL\Types\VarDateTimeType');
+        }
+        $this->_type = Type::getType('vardatetime');
+    }
+
+    public function testDateTimeConvertsToDatabaseValue()
+    {
+        $date = new \DateTime('1985-09-01 10:10:10');
+
+        $expected = $date->format($this->_platform->getDateTimeTzFormatString());
+        $actual = is_string($this->_type->convertToDatabaseValue($date, $this->_platform));
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testDateTimeConvertsToPHPValue()
+    {
+        // Birthday of jwage and also birthday of Doctrine. Send him a present ;)
+        $date = $this->_type->convertToPHPValue('1985-09-01 00:00:00', $this->_platform);
+        $this->assertType('DateTime', $date);
+        $this->assertEquals('1985-09-01 00:00:00', $date->format('Y-m-d H:i:s'));
+        $this->assertEquals('000000', $date->format('u'));
+    }
+
+    public function testInvalidDateTimeFormatConversion()
+    {
+        $this->setExpectedException('Doctrine\DBAL\Types\ConversionException');
+        $this->_type->convertToPHPValue('abcdefg', $this->_platform);
+    }
+
+    public function testConversionWithMicroseconds()
+    {
+        $date = $this->_type->convertToPHPValue('1985-09-01 00:00:00.123456', $this->_platform);
+        $this->assertType('DateTime', $date);
+        $this->assertEquals('1985-09-01 00:00:00', $date->format('Y-m-d H:i:s'));
+        $this->assertEquals('123456', $date->format('u'));
+    }
+
+    public function testNullConversion()
+    {
+        $this->assertNull($this->_type->convertToPHPValue(null, $this->_platform));
+    }
+}
