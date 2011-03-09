2c6eee07a26d453e16551cc5fd11df2ca9b53796
Tom <tom@mail.wordpress.localhost>
2011-03-09 12:22:27 +0000

    AbstractSchemaManager->listTables() can accepts null

--- lib/Doctrine/DBAL/Schema/AbstractSchemaManager.php
+++ lib/Doctrine/DBAL/Schema/AbstractSchemaManager.php
@@ -201,7 +201,7 @@ abstract class AbstractSchemaManager
      *
      * @return Table[]
      */
-    public function listTables(array $classes)
+    public function listTables($classes)
     {
         if(!empty($classes)){
 
