b3cd87c0a93133678fa623c8405a9ebd38f8d6e4
excelwebzone <excelwebzone@gmail.com>
2011-08-01 17:38:28 -0700

    When convertToDatabaseValue make sure the value is type \DateTime

--- lib/Doctrine/DBAL/Types/DateTimeType.php
+++ lib/Doctrine/DBAL/Types/DateTimeType.php
@@ -40,7 +40,7 @@ class DateTimeType extends Type
 
     public function convertToDatabaseValue($value, AbstractPlatform $platform)
     {
-        return ($value !== null)
+        return ($value !== null && $value instanceof \DateTime)
             ? $value->format($platform->getDateTimeFormatString()) : null;
     }
     
