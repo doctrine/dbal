0aa4fcccf0abbf6526fe00733c2d4a4ec7c9e3e2
rodmen <rodmen@gmail.com>
2011-11-30 16:34:32 -0200

    Escape fields for prevent errors using reserved words like read

--- lib/Doctrine/DBAL/Platforms/MySqlPlatform.php
+++ lib/Doctrine/DBAL/Platforms/MySqlPlatform.php
@@ -341,7 +341,20 @@ class MySqlPlatform extends AbstractPlatform
     {
         return 'DROP DATABASE ' . $name;
     }
-
+    /**
+     * Get declaration if a number of fields in bulk
+     * 
+     * @param string $name   name the field to be declared.
+     * @param array  $field  associative array with the name of the properties
+     *
+     * @see AbstractPlatform::getColumnDeclarationSQL()
+     *
+     * @return string
+     */
+    public function getColumnDeclarationSQL($name, array $field)
+    {
+        return parent::getColumnDeclarationSQL(sprintf('`%s`', trim($name, '`')), $field);
+    }
     /**
      * create a new table
      *
