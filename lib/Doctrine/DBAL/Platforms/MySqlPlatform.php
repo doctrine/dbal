04b425e9a2d47c3d8c8b6d844baacdc2166611b5
Benjamin Eberlei <kontakt@beberlei.de>
2012-03-14 21:25:29 +0100

    [DBAL-237] Fix bug when removing and adding an index on the same columns that may be part of a foreign key.

--- lib/Doctrine/DBAL/Platforms/MySqlPlatform.php
+++ lib/Doctrine/DBAL/Platforms/MySqlPlatform.php
@@ -520,6 +520,64 @@ class MySqlPlatform extends AbstractPlatform
     }
 
     /**
+     * Fix for DROP/CREATE index after foreign key change from OneToOne to ManyToOne
+     *
+     * @param TableDiff $diff
+     * @return array
+     */
+    protected function getPreAlterTableIndexForeignKeySQL(TableDiff $diff)
+    {
+        $sql = array();
+        $table = $diff->name;
+
+        foreach ($diff->removedIndexes AS $remKey => $remIndex) {
+
+            foreach ($diff->addedIndexes as $addKey => $addIndex) {
+                if ($remIndex->getColumns() == $addIndex->getColumns()) {
+
+                    $columns = $addIndex->getColumns();
+                    $type = '';
+                    if ($addIndex->isUnique()) {
+                        $type = 'UNIQUE ';
+                    }
+
+                    $query = 'ALTER TABLE ' . $table . ' DROP INDEX ' . $remIndex->getName() . ', ';
+                    $query .= 'ADD ' . $type . 'INDEX ' . $addIndex->getName();
+                    $query .= ' (' . $this->getIndexFieldDeclarationListSQL($columns) . ')';
+
+                    $sql[] = $query;
+
+                    unset($diff->removedIndexes[$remKey]);
+                    unset($diff->addedIndexes[$addKey]);
+
+                    break;
+                }
+            }
+        }
+
+        $sql = array_merge($sql, parent::getPreAlterTableIndexForeignKeySQL($diff));
+
+        return $sql;
+    }
+
+
+    /**
+     * @override
+     */
+    protected function getCreateIndexSQLFlags(Index $index)
+    {
+        $type = '';
+        if ($index->isUnique()) {
+            $type .= 'UNIQUE ';
+        } else if ($index->hasFlag('fulltext')) {
+            $type .= 'FULLTEXT ';
+        }
+
+        return $type;
+    }
+
+
+    /**
      * Obtain DBMS specific SQL code portion needed to declare an integer type
      * field to be used in statements like CREATE TABLE.
      *
