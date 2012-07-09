d0352d7cf8f0b2b5525f18a03617f827c7fc77c3
Danny Berger <dpb587@gmail.com>
2012-07-09 18:01:55 -0400

    Fix DebugStack->stopQuery to prevent unnecessary timings when disabled.

--- lib/Doctrine/DBAL/Logging/DebugStack.php
+++ lib/Doctrine/DBAL/Logging/DebugStack.php
@@ -61,7 +61,8 @@ class DebugStack implements SQLLogger
      */
     public function stopQuery()
     {
-        $this->queries[$this->currentQuery]['executionMS'] = microtime(true) - $this->start;
+        if ($this->enabled) {
+            $this->queries[$this->currentQuery]['executionMS'] = microtime(true) - $this->start;
+        }
     }
 }
-
