7e3dec87af812375face539ab5c2dba40bd52469
Christian Stoller <stoller@leonex.de>
2012-09-13 15:36:38 +0200

    Bugfix DDC-2013 - Default Session Value ".," for "NLS_NUMERIC_CHARACTERS". This is needed because Oracle uses other characters for decimal separation depending on the servers locale

--- lib/Doctrine/DBAL/Event/Listeners/OracleSessionInit.php
+++ lib/Doctrine/DBAL/Event/Listeners/OracleSessionInit.php
@@ -45,6 +45,7 @@ class OracleSessionInit implements EventSubscriber
         'NLS_DATE_FORMAT' => "YYYY-MM-DD HH24:MI:SS",
         'NLS_TIMESTAMP_FORMAT' => "YYYY-MM-DD HH24:MI:SS",
         'NLS_TIMESTAMP_TZ_FORMAT' => "YYYY-MM-DD HH24:MI:SS TZH:TZM",
+        'NLS_NUMERIC_CHARACTERS' => ".,",
     );
 
     /**
