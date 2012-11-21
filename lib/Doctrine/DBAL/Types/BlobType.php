c1d883d39f1d14e7f329cdf66f33c52c9222c7a9
Mike Meier <sk.mykon@gmail.com>
2012-11-21 20:03:58 +0100

    Update lib/Doctrine/DBAL/Types/BlobType.php
    If allow_url_fopen isnt enabled, fopen will return false.

--- lib/Doctrine/DBAL/Types/BlobType.php
+++ lib/Doctrine/DBAL/Types/BlobType.php
@@ -47,11 +47,15 @@ class BlobType extends Type
         if (null === $value) {
             return null;
         }
+
         if (is_string($value)) {
             $value = fopen('data://text/plain;base64,' . base64_encode($value), 'r');
-        } else if ( ! is_resource($value)) {
+        } 
+        
+        if ( ! is_resource($value)) {
             throw ConversionException::conversionFailed($value, self::BLOB);
         }
+
         return $value;
     }
 
