328d8a5387be1fc365f3bba27327023afdee30c5
Benjamin Eberlei <kontakt@beberlei.de>
2013-01-13 09:47:01 +0100

    Change DBAL testing setup to work with Composer, keep the submodules for BC

--- tests/Doctrine/Tests/TestInit.php
+++ tests/Doctrine/Tests/TestInit.php
@@ -6,17 +6,8 @@ namespace Doctrine\Tests;
 
 error_reporting(E_ALL | E_STRICT);
 
-require_once __DIR__ . '/../../../lib/vendor/doctrine-common/lib/Doctrine/Common/ClassLoader.php';
-
-$classLoader = new \Doctrine\Common\ClassLoader('Doctrine\Common', __DIR__ . '/../../../lib/vendor/doctrine-common/lib');
-$classLoader->register();
-
-$classLoader = new \Doctrine\Common\ClassLoader('Doctrine\DBAL', __DIR__ . '/../../../lib');
-$classLoader->register();
+require_once __DIR__ . '/../../../vendor/autoload.php';
 
 $classLoader = new \Doctrine\Common\ClassLoader('Doctrine\Tests', __DIR__ . '/../../');
-$classLoader->register();
-
-$classLoader = new \Doctrine\Common\ClassLoader('Symfony', __DIR__ . "/../../../lib/vendor");
 $classLoader->register();
 
