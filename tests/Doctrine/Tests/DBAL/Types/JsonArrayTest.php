c98f5c55ea1d48b0a4386e907660d25272512335
nemekzg <nemek_zg@poczta.onet.pl>
2013-02-20 18:09:14 +0100

    coding standard fixes

--- tests/Doctrine/Tests/DBAL/Types/JsonArrayTest.php
+++ tests/Doctrine/Tests/DBAL/Types/JsonArrayTest.php
@@ -4,30 +4,28 @@ namespace Doctrine\Tests\DBAL\Types;
 
 use Doctrine\DBAL\Types\Type;
 
-require_once __DIR__ . '/../../TestInit.php';
-
 class JsonArrayTest extends \Doctrine\Tests\DbalTestCase
 {
-    protected
-        $_platform,
-        $_type;
+    
+    protected $platform;
+    protected $type;
 
     protected function setUp()
     {
-        $this->_platform = new \Doctrine\Tests\DBAL\Mocks\MockPlatform();
-        $this->_type = Type::getType('json_array');
+        $this->platform = new \Doctrine\Tests\DBAL\Mocks\MockPlatform();
+        $this->type = Type::getType('json_array');
     }
 
     public function testJsonArrayConvertsToDatabaseValue()
     {
-        $value = $this->_type->convertToDatabaseValue(array(1,2), $this->_platform);
+        $value = $this->type->convertToDatabaseValue(array(1,2), $this->platform);
         $this->assertInternalType('string', $value);
         $this->assertEquals('[1,2]', $value);
     }
 
     public function testJsonArrayConvertsToPHPValue()
     {
-        $value = $this->_type->convertToPHPValue('[3,"a"]', $this->_platform);
+        $value = $this->type->convertToPHPValue('[3,"a"]', $this->platform);
         $this->assertInternalType('array', $value);
         $this->assertCount(2, $value);
         $this->assertContains(3, $value);
@@ -36,6 +34,6 @@ class JsonArrayTest extends \Doctrine\Tests\DbalTestCase
 
     public function testJsonArrayNullConvertsToPHPValue()
     {
-        $this->assertNull($this->_type->convertToPHPValue(null, $this->_platform));
+        $this->assertNull($this->type->convertToPHPValue(null, $this->platform));
     }
 }
