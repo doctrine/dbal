efb6da3dfd702ffb3fc8a59264648bdc1c7950ba
Philipp Scheit <p.scheit@ps-webforge.com>
2013-04-23 11:21:58 +0300

    fix function name in exception text

--- lib/Doctrine/DBAL/DBALException.php
+++ lib/Doctrine/DBAL/DBALException.php
@@ -91,7 +91,7 @@ class DBALException extends \Exception
     {
         return new self('Unknown column type "'.$name.'" requested. Any Doctrine type that you use has ' .
             'to be registered with \Doctrine\DBAL\Types\Type::addType(). You can get a list of all the ' .
-            'known types with \Doctrine\DBAL\Types\Type::getTypeMap(). If this error occurs during database ' .
+            'known types with \Doctrine\DBAL\Types\Type::getTypesMap(). If this error occurs during database ' .
             'introspection then you might have forgot to register all database types for a Doctrine Type. Use ' .
             'AbstractPlatform#registerDoctrineTypeMapping() or have your custom types implement ' .
             'Type#getMappedDatabaseTypes(). If the type name is empty you might ' .
