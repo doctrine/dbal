nextcloud/3rdparty

Dropped indexes can have constraints, drop those first if they exist

doctrine/dbal/lib/Doctrine/DBAL/Platforms/PostgreSqlPlatform.php
@@ -435,6 +435,27 @@ class PostgreSqlPlatform extends AbstractPlatform
         return array_merge($sql, $tableSql, $columnSql);
     }
 
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function getPreAlterTableIndexForeignKeySQL(TableDiff $diff)
+    {
+        $sql = array();
+        $table = $diff->name;
+        foreach ($diff->changedIndexes as $changedKey => $changedIndex) {
+
+                    $sql[] = $this->getDropConstraintSQL('IF EXISTS '. $changedIndex->getName(), $table);
+                    $sql[] = $this->getDropIndexSQL('IF EXISTS '. $changedIndex->getName(), $table);
+                    $diff->addedIndexes[$changedKey] = $diff->changedIndexes[$changedKey];
+                    unset($diff->changedIndexes[$changedKey]);
+        }
+
+        $sql = array_merge($sql, parent::getPreAlterTableIndexForeignKeySQL($diff));
+
+        return $sql;
+    }
+
     /**
      * {@inheritDoc}
      */

doctrine/dbal

lib/Doctrine/DBAL/Platforms/PostgreSqlPlatform.php
@@ -507,6 +507,25 @@ class PostgreSqlPlatform extends AbstractPlatform
     }
 
     /**
+     * {@inheritDoc}
+     */
+    protected function getPreAlterTableIndexForeignKeySQL(TableDiff $diff)
+    {
+        $sql = array();
+        $table = $diff->name;
+        foreach ($diff->changedIndexes as $changedKey => $changedIndex) {
+            $sql[] = $this->getDropConstraintSQL('IF EXISTS ' . $changedIndex->getName(), $table);
+            $sql[] = $this->getDropIndexSQL('IF EXISTS ' . $changedIndex->getName(), $table);
+            $diff->addedIndexes[$changedKey] = $diff->changedIndexes[$changedKey];
+            unset($diff->changedIndexes[$changedKey]);
+        }
+
+        $sql = array_merge($sql, parent::getPreAlterTableIndexForeignKeySQL($diff));
+
+        return $sql;
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function getCommentOnColumnSQL($tableName, $columnName, $comment)