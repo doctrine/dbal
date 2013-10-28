b12db241aa4848a0ee7067b7a70e0fb306e375b2
till <till@php.net>
2013-10-28 15:46:09 +0100

    Enhancement: small test case
     * test case to assert the connection timeout is set
     * test case to assert on exceptions thrown
       * wip, as it seems impossible to trigger 'false' from mysqli_options

---
+++ tests/Doctrine/Tests/DBAL/Functional/Mysqli/ConnectionTest.php
@@ -0,0 +1,54 @@
+<?php
+namespace Doctrine\Tests\DBAL\Functional\Mysqli;
+
+require_once __DIR__ . '/../../../TestInit.php';
+
+class ConnectionTest extends \Doctrine\Tests\DbalFunctionalTestCase
+{
+    public function setUp()
+    {
+        if (!extension_loaded('mysqli')) {
+            $this->markTestSkipped('mysqli is not installed.');
+        }
+
+        $this->resetSharedConn();
+        parent::setUp();
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+        $this->resetSharedConn();
+    }
+
+    public function testDriverOptions()
+    {
+        $driverOptions = array(
+            \MYSQLI_OPT_CONNECT_TIMEOUT => 1,
+        );
+
+        $connection = $this->getConnection($driverOptions);
+        $this->assertInstanceOf("\Doctrine\DBAL\Driver\Mysqli\MysqliConnection", $connection);
+    }
+
+    /**
+     * @expectedException \Doctrine\DBAL\Driver\Mysqli\MysqliException
+     */
+    public function testUnsupportedDriverOption()
+    {
+        $this->getConnection(array('hello' => 'world')); // use local infile
+    }
+
+    private function getConnection(array $driverOptions)
+    {
+        return new \Doctrine\DBAL\Driver\Mysqli\MysqliConnection(
+            array(
+                 'host' => $GLOBALS['db_host'],
+                 'dbname' => $GLOBALS['db_name'],
+            ),
+            $GLOBALS['db_username'],
+            $GLOBALS['db_password'],
+            $driverOptions
+        );
+    }
+}
