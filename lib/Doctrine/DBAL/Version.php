328357bd9eea9d671fe5fff0737f01953bfe66a0
Benjamin Eberlei <kontakt@beberlei.de>
2013-11-12 13:38:28 +0100

    Release 2.4.1

--- lib/Doctrine/DBAL/Version.php
+++ lib/Doctrine/DBAL/Version.php
@@ -34,7 +34,7 @@ class Version
     /**
      * Current Doctrine Version.
      */
-    const VERSION = '2.4.1-DEV';
+    const VERSION = '2.4.1';
 
     /**
      * Compares a Doctrine version with the current one.
