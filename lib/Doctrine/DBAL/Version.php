c3448dc0a163becd5acb50199049996c53cade78
Benjamin Eberlei <kontakt@beberlei.de>
2013-11-12 13:38:29 +0100

    Bump version to 2.4.2

--- lib/Doctrine/DBAL/Version.php
+++ lib/Doctrine/DBAL/Version.php
@@ -34,7 +34,7 @@ class Version
     /**
      * Current Doctrine Version.
      */
-    const VERSION = '2.4.1';
+    const VERSION = '2.4.2-DEV';
 
     /**
      * Compares a Doctrine version with the current one.
