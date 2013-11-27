d00511cde2fa4e298ac9abfe9dbc60ab50971b18
Steve Müller <st.mueller@dzh-online.de>
2013-11-27 18:25:31 +0100

    fix binding LOB values in mysqli driver prepared statement

--- lib/Doctrine/DBAL/Driver/Mysqli/MysqliStatement.php
+++ lib/Doctrine/DBAL/Driver/Mysqli/MysqliStatement.php
@@ -165,6 +165,11 @@ class MysqliStatement implements \IteratorAggregate, Statement
             throw new MysqliException($this->_stmt->error, $this->_stmt->errno);
         }
 
+        // We have a result.
+        if (false !== $this->_columnNames) {
+            $this->_stmt->store_result();
+        }
+
         if (null === $this->_columnNames) {
             $meta = $this->_stmt->result_metadata();
             if (false !== $meta) {
@@ -188,11 +193,6 @@ class MysqliStatement implements \IteratorAggregate, Statement
             } else {
                 $this->_columnNames = false;
             }
-        }
-
-        // We have a result.
-        if (false !== $this->_columnNames) {
-            $this->_stmt->store_result();
         }
 
         return true;
