76af9c69d90cc0b2e20c525e1475c0a07cf59383
Benjamin Eberlei <kontakt@beberlei.de>
2013-12-20 17:10:51 +0100

    Fix 2.4 tests on MySQL

--- tests/Doctrine/Tests/DBAL/Functional/Schema/MySqlSchemaManagerTest.php
+++ tests/Doctrine/Tests/DBAL/Functional/Schema/MySqlSchemaManagerTest.php
@@ -4,8 +4,7 @@ namespace Doctrine\Tests\DBAL\Functional\Schema;
 
 use Doctrine\DBAL\Schema\Table;
 use Doctrine\DBAL\Schema\Schema;
-
-require_once __DIR__ . '/../../../TestInit.php';
+use Doctrine\DBAL\Schema\Comparator;
 
 class MySqlSchemaManagerTest extends SchemaManagerFunctionalTestCase
 {
