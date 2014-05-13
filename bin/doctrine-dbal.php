4b9da747256b4b6da37873dd5c95372d1f4ac928
Marco Pivetta <ocramius@gmail.com>
2014-05-14 01:01:14 +0200

    #598 - Minor CS/cleanups in the DBAL CLI runner

--- bin/doctrine-dbal.php
+++ bin/doctrine-dbal.php
@@ -20,7 +20,11 @@
 use Symfony\Component\Console\Helper\HelperSet;
 use Doctrine\DBAL\Tools\Console\ConsoleRunner;
 
-$files = array(__DIR__ . '/../vendor/autoload.php', __DIR__ . '/../../../autoload.php');
+$files       = array(__DIR__ . '/../vendor/autoload.php', __DIR__ . '/../../../autoload.php');
+$loader      = null;
+$cwd         = getcwd();
+$directories = array($cwd, $cwd . DIRECTORY_SEPARATOR . 'config');
+$configFile  = null;
 
 foreach ($files as $file) {
     if (file_exists($file)) {
@@ -30,13 +34,10 @@ foreach ($files as $file) {
     }
 }
 
-if (! $loader) {
+if ( ! $loader) {
     throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
 }
 
-$directories = array(getcwd(), getcwd() . DIRECTORY_SEPARATOR . 'config');
-
-$configFile = null;
 foreach ($directories as $directory) {
     $configFile = $directory . DIRECTORY_SEPARATOR . 'cli-config.php';
 
@@ -47,22 +48,24 @@ foreach ($directories as $directory) {
 
 if ( ! file_exists($configFile)) {
     ConsoleRunner::printCliConfigTemplate();
+
     exit(1);
 }
 
 if ( ! is_readable($configFile)) {
     echo 'Configuration file [' . $configFile . '] does not have read permission.' . PHP_EOL;
+
     exit(1);
 }
 
-$commands = array();
-
+$commands  = array();
 $helperSet = require $configFile;
 
-if ( ! ($helperSet instanceof HelperSet)) {
+if ( ! $helperSet instanceof HelperSet) {
     foreach ($GLOBALS as $helperSetCandidate) {
         if ($helperSetCandidate instanceof HelperSet) {
             $helperSet = $helperSetCandidate;
+
             break;
         }
     }
