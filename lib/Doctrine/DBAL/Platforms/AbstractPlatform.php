d82d067a94de6d9ea321f25d528de0709fd8faf0
Steve Müller <st.mueller@dzh-online.de>
2014-08-21 16:29:49 +0200

    #657 - add note about the intention of platform's string literal quotation method

--- lib/Doctrine/DBAL/Platforms/AbstractPlatform.php
+++ lib/Doctrine/DBAL/Platforms/AbstractPlatform.php
@@ -3488,7 +3488,10 @@ abstract class AbstractPlatform
     }
 
     /**
-     * Quotes a literal string
+     * Quotes a literal string.
+     * This method is NOT meant to fix SQL injections!
+     * It is only meant to escape this platform's string literal
+     * quote character inside the given literal string.
      *
      * @param string $str The literal string to be quoted.
      *
