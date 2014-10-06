e5b80ff18f7ebaa09566733025ecc5a1cf7dd8d3
ChristopheBoucaut <boucaut.christophe@gmail.com>
2014-10-06 12:50:33 +0200

    Update MasterSlaveConnection.php
    If you use close method just before connect method, $this->_conn is unset and we have got an error php.

--- lib/Doctrine/DBAL/Connections/MasterSlaveConnection.php
+++ lib/Doctrine/DBAL/Connections/MasterSlaveConnection.php
@@ -151,7 +151,7 @@ class MasterSlaveConnection extends Connection
         // If we have a connection open, and this is not an explicit connection
         // change request, then abort right here, because we are already done.
         // This prevents writes to the slave in case of "keepSlave" option enabled.
-        if ($this->_conn && !$requestedConnectionChange) {
+        if (isset($this->_conn) && $this->_conn && !$requestedConnectionChange) {
             return false;
         }
 
