c5e5d5921f4efadd97740da23ffb915e9ac37258
Guilherme Blanco <guilhermeblanco@gmail.com>
2015-01-11 10:46:37 -0500

    Moved Doctrine\Tests namespace to composer autoload-dev.

--- tests/Doctrine/Tests/TestInit.php
+++ tests/Doctrine/Tests/TestInit.php
@@ -8,14 +8,10 @@ date_default_timezone_set('UTC');
 
 if (file_exists(__DIR__ . '/../../../vendor/autoload.php')) {
     // dependencies were installed via composer - this is the main project
-    $classLoader = require __DIR__ . '/../../../vendor/autoload.php';
+    require __DIR__ . '/../../../vendor/autoload.php';
 } elseif (file_exists(__DIR__ . '/../../../../../autoload.php')) {
     // installed as a dependency in `vendor`
-    $classLoader = require __DIR__ . '/../../../../../autoload.php';
+    require __DIR__ . '/../../../../../autoload.php';
 } else {
     throw new Exception('Can\'t find autoload.php. Did you install dependencies via Composer?');
 }
-
-/* @var $classLoader \Composer\Autoload\ClassLoader */
-$classLoader->add('Doctrine\\Tests\\', __DIR__ . '/../../');
-unset($classLoader);
