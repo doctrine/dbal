8bdd3af5b1c4195cb5ba922cc751c1f1ccf5229c
jarekj <jarekj6@gmail.com>
2015-01-29 17:28:24 +0100

    Update QueryBuilder.php

--- lib/Doctrine/DBAL/Query/QueryBuilder.php
+++ lib/Doctrine/DBAL/Query/QueryBuilder.php
@@ -1308,10 +1308,10 @@ class QueryBuilder
         $sql = '';
 
         if (isset($this->sqlParts['join'][$fromAlias])) {
-            if (array_key_exists($join['joinAlias'], $knownAliases)) {
-                throw QueryException::notUniqueAlias($join['joinAlias'], $knownAliases);
-            }
             foreach ($this->sqlParts['join'][$fromAlias] as $join) {
+                if (array_key_exists($join['joinAlias'], $knownAliases)) {
+                    throw QueryException::notUniqueAlias($join['joinAlias'], $knownAliases);
+                }
                 $sql .= ' ' . strtoupper($join['joinType'])
                     . ' JOIN ' . $join['joinTable'] . ' ' . $join['joinAlias']
                     . ' ON ' . ((string) $join['joinCondition']);
