f06e12661834f83854f25769280f589af6b1fd21
jarekj <jarekj6@gmail.com>
2015-01-30 10:01:24 +0100

    Update QueryBuilderTest.php

--- tests/Doctrine/Tests/DBAL/Query/QueryBuilderTest.php
+++ tests/Doctrine/Tests/DBAL/Query/QueryBuilderTest.php
@@ -863,4 +863,24 @@ class QueryBuilderTest extends \Doctrine\Tests\DbalTestCase
 
         $this->assertSame(array('name' => \PDO::PARAM_STR, 'isActive' => \PDO::PARAM_BOOL), $qb->getParameterTypes());
     }
+
+    /**
+     * @group DBAL-790
+     */
+    public function testSelectWithJoinsWithNotUniqueAliases()
+    {
+        $qb = new QueryBuilder($this->conn);
+
+        $qb->select('a.id, b.id')
+            ->from('table_a', 'a')
+            ->join('a', 'table_b', 'b', 'a.fk_b = b.id')
+            ->join('a', 'table_b', 'b', 'a.fk_b = b.id')
+            ->join('b', 'table_a', 'a', 'a.fk_b = b.id');
+
+        $this->setExpectedException('QueryException');
+        $this->assertEquals(
+            'dumb string',
+            $qb->getSQL(),
+        );
+    }
 }
