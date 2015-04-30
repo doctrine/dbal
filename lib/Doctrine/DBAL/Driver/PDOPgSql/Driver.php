d838d800dd21dba25b37638acaffe53f4f09a0c2
Steve Müller <st.mueller@dzh-online.de>
2015-04-30 14:34:49 +0200

    add comment about using template1 database if dbname parameter is not set

--- lib/Doctrine/DBAL/Driver/PDOPgSql/Driver.php
+++ lib/Doctrine/DBAL/Driver/PDOPgSql/Driver.php
@@ -60,6 +60,9 @@ class Driver implements \Doctrine\DBAL\Driver
         if (isset($params['dbname'])) {
             $dsn .= 'dbname=' . $params['dbname'] . ' ';
         } else {
+            // Used for temporary connections to allow operations like dropping the database currently connected to.
+            // Connecting without an explicit database does not work, therefore "template1" database is used
+            // as it is certainly present in every server setup.
             $dsn .= 'dbname=template1' . ' ';
         }
 
