bd9931ea0bff74b965626e9ff721c695c4b0b24c
Valentinas Bartusevičius <v.bartusevicius@evp.lt>
2015-05-09 14:39:37 +0300

    Added Tests to DateInterval Type

--- lib/Doctrine/DBAL/Types/DateIntervalType.php
+++ lib/Doctrine/DBAL/Types/DateIntervalType.php
@@ -3,10 +3,54 @@
 
 namespace Doctrine\DBAL\Types;
 
-/**
- * 
- */
-class DateIntervalType 
-{
+use Doctrine\DBAL\Platforms\AbstractPlatform;
 
+/**
+ * Type that maps interval string to a PHP DateInterval Object.
+ */
+class DateIntervalType extends Type
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return Type::DATEINTERVAL;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        return $platform->getVarcharTypeDeclarationSQL($fieldDeclaration);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        return ($value !== null)
+            ? $value->format('P%yY%mM%dDT%hH%iM%sS') : null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        if ($value === null || $value instanceof \DateInterval) {
+            return $value;
+        }
+
+        try {
+            $interval = new \DateInterval($value);
+        } catch (\Exception $e) {
+            throw ConversionException::conversionFailedFormat($value, $this->getName(), 'PxYxMxDTxHxMxS');
+
+        }
+
+        return $interval;
+    }
 }
