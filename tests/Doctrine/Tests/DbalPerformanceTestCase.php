60333c040d544fadc9637abc1a60c6bf66bd2d6e
Bill Schaller <bill@zeroedin.com>
2015-06-17 12:24:44 -0400

    Add DbalPerformanceTestCase

---
+++ tests/Doctrine/Tests/DbalPerformanceTestCase.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Doctrine\Tests;
+
+/**
+ * Class DbalPerformanceTestCase
+ * @package Doctrine\Tests\DBAL
+ * @author Bill Schaller
+ * @author robo
+ */
+class DbalPerformanceTestCase extends DbalFunctionalTestCase
+{
+    /**
+     * @var integer
+     */
+    protected $maxRunningTime = 0;
+
+    /**
+     * @return void
+     */
+    protected function runTest()
+    {
+        $s = microtime(true);
+        parent::runTest();
+        $time = microtime(true) - $s;
+
+        if ($this->maxRunningTime != 0 && $time > $this->maxRunningTime) {
+            $this->fail(
+                sprintf(
+                    'expected running time: <= %s but was: %s',
+
+                    $this->maxRunningTime,
+                    $time
+                )
+            );
+        }
+    }
+
+    /**
+     * @param integer $maxRunningTime
+     *
+     * @return void
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function setMaxRunningTime($maxRunningTime)
+    {
+        if (is_integer($maxRunningTime) && $maxRunningTime >= 0) {
+            $this->maxRunningTime = $maxRunningTime;
+        } else {
+            throw new \InvalidArgumentException;
+        }
+    }
+
+    /**
+     * @return integer
+     */
+    public function getMaxRunningTime()
+    {
+        return $this->maxRunningTime;
+    }
+}
