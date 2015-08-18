062a779946bc3883202c4fe1664ddc2336c98cf8
Vincent CHALAMON <vincentchalamon@gmail.com>
2015-08-18 13:59:00 +0200

    Override methods for sharding connection

--- lib/Doctrine/DBAL/Sharding/PoolingShardManager.php
+++ lib/Doctrine/DBAL/Sharding/PoolingShardManager.php
@@ -19,6 +19,8 @@
 
 namespace Doctrine\DBAL\Sharding;
 
+use Doctrine\DBAL\Sharding\ShardChoser\ShardChoser;
+
 /**
  * Shard Manager for the Connection Pooling Shard Strategy
  *
@@ -27,12 +29,12 @@ namespace Doctrine\DBAL\Sharding;
 class PoolingShardManager implements ShardManager
 {
     /**
-     * @var \Doctrine\DBAL\Sharding\PoolingShardConnection
+     * @var PoolingShardConnection
      */
     private $conn;
 
     /**
-     * @var \Doctrine\DBAL\Sharding\ShardChoser\ShardChoser
+     * @var ShardChoser
      */
     private $choser;
 
@@ -42,7 +44,7 @@ class PoolingShardManager implements ShardManager
     private $currentDistributionValue;
 
     /**
-     * @param \Doctrine\DBAL\Sharding\PoolingShardConnection $conn
+     * @param PoolingShardConnection $conn
      */
     public function __construct(PoolingShardConnection $conn)
     {
