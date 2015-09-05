e5202c0f7734be7e9fe3e57cf15cd9065dbc06f9
Marco Pivetta <ocramius@gmail.com>
2015-09-05 18:11:37 +0200

    #869 - DBAL-1293 - Using a data provider for invalid type conversions

--- tests/Doctrine/Tests/DBAL/Types/TimeTest.php
+++ tests/Doctrine/Tests/DBAL/Types/TimeTest.php
@@ -30,18 +30,15 @@ class TimeTest extends \Doctrine\Tests\DbalTestCase
         );
     }
 
-    public function testInvalidDateTimeValueInteger()
+    /**
+     * @dataProvider invalidPHPValuesProvider
+     * @param mixed $value
+     */
+    public function testInvalidTypeConversionToDatabaseValue($value)
     {
         $this->setExpectedException('Doctrine\DBAL\Types\ConversionException');
 
-        $this->_type->convertToDatabaseValue(27, $this->_platform);
-    }
-
-    public function testInvalidDateTimeValueStdClass()
-    {
-        $this->setExpectedException('Doctrine\DBAL\Types\ConversionException');
-
-        $this->_type->convertToDatabaseValue(new \stdClass(), $this->_platform);
+        $this->_type->convertToDatabaseValue($value, $this->_platform);
     }
 
     public function testTimeConvertsToPHPValue()
@@ -74,5 +71,23 @@ class TimeTest extends \Doctrine\Tests\DbalTestCase
     {
         $date = new \DateTime("now");
         $this->assertSame($date, $this->_type->convertToPHPValue($date, $this->_platform));
+    }
+
+    /**
+     * @return mixed[][]
+     */
+    public function invalidPHPValuesProvider()
+    {
+        return [
+            [0],
+            [''],
+            ['foo'],
+            ['10:11:12'],
+            [new \stdClass()],
+            [$this],
+            [27],
+            [-1],
+            [1.2],
+        ];
     }
 }
