34b0c9e6df50c7eff71257a9c8670e69efc1d439
Steve Müller <st.mueller@dzh-online.de>
2015-09-16 18:29:38 +0200

    Bump version to 2.5.3

--- lib/Doctrine/DBAL/Version.php
+++ lib/Doctrine/DBAL/Version.php
@@ -34,7 +34,7 @@ class Version
     /**
      * Current Doctrine Version.
      */
-    const VERSION = '2.5.2';
+    const VERSION = '2.5.3-DEV';
 
     /**
      * Compares a Doctrine version with the current one.
