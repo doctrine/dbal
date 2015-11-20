8a3a81f9ef0c9c37b9d6a0fe7c664c56308588aa
Mathieu Rochette <mathieu@rochette.cc>
2015-11-20 23:47:58 +0100

    Avoid rewrapping Docrine\DBAL\Exception\DriverException

--- tests/Doctrine/Tests/DBAL/DBALExceptionTest.php
+++ tests/Doctrine/Tests/DBAL/DBALExceptionTest.php
@@ -3,6 +3,7 @@
 namespace Doctrine\Tests\DBAL;
 
 use Doctrine\DBAL\DBALException;
+use Doctrine\DBAL\Exception\DriverException;
 
 class DBALExceptionTest extends \Doctrine\Tests\DbalTestCase
 {
@@ -11,5 +12,13 @@ class DBALExceptionTest extends \Doctrine\Tests\DbalTestCase
         $driver = $this->getMock('\Doctrine\DBAL\Driver');
         $e = DBALException::driverExceptionDuringQuery($driver, new \Exception, '', array('ABC', chr(128)));
         $this->assertContains('with params ["ABC", "\x80"]', $e->getMessage());
+    }
+
+    public function testAvoidOverWrappingOnDriverException()
+    {
+        $driver = $this->getMock('\Doctrine\DBAL\Driver');
+        $ex = new DriverException('', $this->getMock('\Doctrine\DBAL\Driver\DriverException'));
+        $e = DBALException::driverExceptionDuringQuery($driver, $ex, '');
+        $this->assertSame($ex, $e);
     }
 }
