3af9d3f6d7d901ce958dab61b928c0116e424c9a
Marco Pivetta <ocramius@gmail.com>
2016-01-05 23:12:10 +0100

    Bumping version to 2.5.5-DEV

--- lib/Doctrine/DBAL/Version.php
+++ lib/Doctrine/DBAL/Version.php
@@ -34,7 +34,7 @@ class Version
     /**
      * Current Doctrine Version.
      */
-    const VERSION = '2.5.4';
+    const VERSION = '2.5.5-DEV';
 
     /**
      * Compares a Doctrine version with the current one.
