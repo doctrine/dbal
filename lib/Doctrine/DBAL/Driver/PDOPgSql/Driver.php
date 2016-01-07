af38e30ab9c5828b20312d796651d192020d18c1
Kim Hemsø Rasmussen <kimhemsoe@gmail.com>
2016-01-07 02:16:05 +0100

    Stop using template1 as default database for postgres drivers

--- lib/Doctrine/DBAL/Driver/PDOPgSql/Driver.php
+++ lib/Doctrine/DBAL/Driver/PDOPgSql/Driver.php
@@ -90,9 +90,9 @@ class Driver extends AbstractPostgreSQLDriver
             $dsn .= 'dbname=' . $params['dbname'] . ' ';
         } else {
             // Used for temporary connections to allow operations like dropping the database currently connected to.
-            // Connecting without an explicit database does not work, therefore "template1" database is used
-            // as it is certainly present in every server setup.
-            $dsn .= 'dbname=template1' . ' ';
+            // Connecting without an explicit database does not work, therefore "postgres" database is used
+            // as it is certainly present in every server setup. 
+            $dsn .= 'dbname=postgres' . ' ';
         }
 
         if (isset($params['sslmode'])) {
