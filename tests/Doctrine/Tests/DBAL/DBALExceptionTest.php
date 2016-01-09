0f13e3b110ed8c7eb31e12ee5ac477aec5d3e3f4
Steve Müller <st.mueller@dzh-online.de>
2016-01-09 16:19:14 +0100

    trim leading slash from schemeless connection URL path with default driver connection parameters
    - includes extended default driver connection parameter handling
    - minor refactoring
    
    fixes #1183

--- tests/Doctrine/Tests/DBAL/DBALExceptionTest.php
+++ tests/Doctrine/Tests/DBAL/DBALExceptionTest.php
@@ -12,4 +12,28 @@ class DBALExceptionTest extends \Doctrine\Tests\DbalTestCase
         $e = DBALException::driverExceptionDuringQuery($driver, new \Exception, '', array('ABC', chr(128)));
         $this->assertContains('with params ["ABC", "\x80"]', $e->getMessage());
     }
+
+    public function testAvoidOverWrappingOnDriverException()
+    {
+        $driver = $this->createMock('\Doctrine\DBAL\Driver');
+        $ex = new DriverException('', $this->createMock('\Doctrine\DBAL\Driver\DriverException'));
+        $e = DBALException::driverExceptionDuringQuery($driver, $ex, '');
+        $this->assertSame($ex, $e);
+    }
+
+    public function testDriverRequiredWithUrl()
+    {
+        $url = 'mysql://localhost';
+        $exception = DBALException::driverRequired($url);
+
+        $this->assertInstanceOf('Doctrine\DBAL\DBALException', $exception);
+        $this->assertSame(
+            sprintf(
+                "The options 'driver' or 'driverClass' are mandatory if a connection URL without scheme " .
+                "is given to DriverManager::getConnection(). Given URL: %s",
+                $url
+            ),
+            $exception->getMessage()
+        );
+    }
 }
