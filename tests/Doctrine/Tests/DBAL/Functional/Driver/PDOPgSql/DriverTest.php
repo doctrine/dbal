44ab7bcd84af6936bc93c6d2bcab800c96f2941e
Steve Müller <st.mueller@dzh-online.de>
2016-01-10 04:10:53 +0100

    fix test for Travis

--- tests/Doctrine/Tests/DBAL/Functional/Driver/PDOPgSql/DriverTest.php
+++ tests/Doctrine/Tests/DBAL/Functional/Driver/PDOPgSql/DriverTest.php
@@ -32,9 +32,21 @@ class DriverTest extends AbstractDriverTest
         $password = isset($parameters['password']) ? $parameters['password'] : null;
 
         $connection = $this->driver->connect($parameters, $user, $password);
-        $statement = $connection->query('SELECT application_name FROM pg_stat_activity');
 
-        $this->assertSame('doctrine', $statement->fetchColumn());
+        $hash = microtime(true); // required to identify the record in the results uniquely
+        $sql = sprintf('SELECT query, application_name FROM pg_stat_activity WHERE %d = %d', $hash, $hash);
+        $statement = $connection->query($sql);
+        $records = $statement->fetchAll();
+
+        foreach ($records as $record) {
+            if ($record['query'] === $sql) {
+                $this->assertSame('doctrine', $record['application_name']);
+
+                return;
+            }
+        }
+
+        $this->fail(sprintf('Query result does not contain a record where column "query" equals "%s".', $sql));
     }
 
     /**
