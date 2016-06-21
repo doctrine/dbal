7929c7f3bdb0280a3a5274b012f6de78b3e50d15
Andreas Pohl <andreas.pohl@mateurope.com>
2016-06-21 09:17:15 +0200

    Corrected PR for persistent SQLAny-Connections
    * removed "charset" patch
    * implemeted resource_type compare with in_array instead of regex
    * implemented functional tests for connection and statement.

---
+++ tests/Doctrine/Tests/DBAL/Functional/Driver/SQLAnywhere/ConnectionTest.php
@@ -0,0 +1,49 @@
+<?php
+namespace Doctrine\Tests\DBAL\Functional\Driver\SQLAnywhere;
+
+use Doctrine\DBAL\DriverManager;
+
+class ConnectionTest extends \Doctrine\Tests\DbalFunctionalTestCase
+{
+    protected function setUp()
+    {
+        if (!extension_loaded('sqlanywhere')) {
+            $this->markTestSkipped('sqlanywhere is not installed.');
+        }
+
+        parent::setUp();
+
+        if ( !($this->_conn->getDriver() instanceof \Doctrine\DBAL\Driver\SQLAnywhere\Driver)) {
+            $this->markTestSkipped('sqlanywhere only test.');
+        }
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    public function testNonPersistentConnection()
+    {
+        $params = $this->_conn->getParams();
+        $params['persistent'] = false;
+
+        $conn = DriverManager::getConnection($params);
+
+        $conn->connect();
+
+        $this->assertTrue($conn->isConnected(), 'No SQLAnywhere-nonpersistent connection established');
+    }
+
+    public function testPersistentConnection()
+    {
+        $params = $this->_conn->getParams();
+        $params['persistent'] = true;
+
+        $conn = DriverManager::getConnection($params);
+
+        $conn->connect();
+
+        $this->assertTrue($conn->isConnected(), 'No SQLAnywhere-persistent connection established');
+    }
+}
