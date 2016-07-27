86c51f57735f1ced2b6648144051ecf38b4fd481
yuki kato <m.yukato@gmail.com>
2016-07-27 23:56:44 +0700

    Exclude spatial extension tables

--- lib/Doctrine/DBAL/Platforms/MySqlPlatform.php
+++ lib/Doctrine/DBAL/Platforms/MySqlPlatform.php
@@ -386,7 +386,7 @@ class MySqlPlatform extends AbstractPlatform
         return "SELECT COLUMN_NAME AS Field, COLUMN_TYPE AS Type, IS_NULLABLE AS `Null`, ".
                "COLUMN_KEY AS `Key`, COLUMN_DEFAULT AS `Default`, EXTRA AS Extra, COLUMN_COMMENT AS Comment, " .
                "CHARACTER_SET_NAME AS CharacterSet, COLLATION_NAME AS Collation ".
-               "FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = " . $database . " AND TABLE_NAME = " . $table;
+               "FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = " . $database . " AND TABLE_NAME = " . $table . " AND DATA_TYPE != 'geometry'";
     }
 
     /**
