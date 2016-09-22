ee858e379e7b7cf2dbd33a7b56c1766c1013b989
Guilherme Blanco <guilhermeblanco@gmail.com>
2016-09-22 22:53:42 +0000

    Added flag support to unique constraint

--- lib/Doctrine/DBAL/Schema/Table.php
+++ lib/Doctrine/DBAL/Schema/Table.php
@@ -121,11 +121,12 @@ class Table extends AbstractAsset
     /**
      * @param mixed[]     $columnNames
      * @param string|null $indexName
+     * @param array       $flags
      * @param mixed[]     $options
      *
      * @return self
      */
-    public function addUniqueConstraint(array $columnNames, $indexName = null, array $options = [])
+    public function addUniqueConstraint(array $columnNames, $indexName = null, array $flags = array(), array $options = [])
     {
         if ($indexName === null) {
             $indexName = $this->_generateIdentifierName(
@@ -135,7 +136,7 @@ class Table extends AbstractAsset
             );
         }
 
-        return $this->_addUniqueConstraint($this->_createUniqueConstraint($columnNames, $indexName, $options));
+        return $this->_addUniqueConstraint($this->_createUniqueConstraint($columnNames, $indexName, $flags, $options));
     }
 
     /**
@@ -864,7 +865,7 @@ class Table extends AbstractAsset
     /**
      * @return self
      */
-    protected function _addUniqueConstraint(UniqueConstraint $uniqueConstraint)
+    protected function _addUniqueConstraint(UniqueConstraint $constraint)
     {
         $name = strlen($uniqueConstraint->getName())
             ? $uniqueConstraint->getName()
@@ -876,7 +877,7 @@ class Table extends AbstractAsset
 
         $name = $this->normalizeIdentifier($name);
 
-        $this->_uniqueConstraints[$name] = $uniqueConstraint;
+        $this->_uniqueConstraints[$name] = $constraint;
 
         // If there is already an index that fulfills this requirements drop the request. In the case of __construct
         // calling this method during hydration from schema-details all the explicitly added indexes lead to duplicates.
@@ -964,13 +965,14 @@ class Table extends AbstractAsset
     /**
      * @param mixed[] $columns
      * @param string  $indexName
-     * @param mixed[] $options
+     * @param mixed[] $flags
+     * @param array  $options
      *
      * @return UniqueConstraint
      *
      * @throws SchemaException
      */
-    private function _createUniqueConstraint(array $columns, $indexName, array $options = [])
+    private function _createUniqueConstraint(array $columns, $indexName, array $flags = array(), array $options = [])
     {
         if (preg_match('(([^a-zA-Z0-9_]+))', $this->normalizeIdentifier($indexName))) {
             throw SchemaException::indexNameInvalid($indexName);
@@ -988,7 +990,7 @@ class Table extends AbstractAsset
             }
         }
 
-        return new UniqueConstraint($indexName, $columns, $options);
+        return new UniqueConstraint($indexName, $columns, $flags, $options);
     }
 
     /**
