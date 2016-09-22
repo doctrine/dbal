cb67105fcbbff99a35101b6a5aa3dec4508348e0
Guilherme Blanco <guilhermeblanco@gmail.com>
2016-09-22 22:53:42 +0000

    Added flag support to unique constraint

--- lib/Doctrine/DBAL/Platforms/SQLServerPlatform.php
+++ lib/Doctrine/DBAL/Platforms/SQLServerPlatform.php
@@ -396,18 +396,6 @@ SQL
     /**
      * {@inheritDoc}
      */
-    public function getUniqueConstraintDeclarationSQL($name, Index $index)
-    {
-        $constraint = parent::getUniqueConstraintDeclarationSQL($name, $index);
-
-        $constraint = $this->_appendUniqueConstraintDefinition($constraint, $index);
-
-        return $constraint;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
     public function getCreateIndexSQL(Index $index, $table)
     {
         $constraint = parent::getCreateIndexSQL($index, $table);
