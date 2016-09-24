7cc737ccec5b9bde8300bb6c7131b4b4aa35a8d7
Guilherme Blanco <guilhermeblanco@gmail.com>
2016-09-24 04:02:45 +0000

    Removed identifier generator type from schema table definition

--- tests/Doctrine/Tests/DBAL/Functional/Schema/SchemaManagerFunctionalTestCase.php
+++ tests/Doctrine/Tests/DBAL/Functional/Schema/SchemaManagerFunctionalTestCase.php
@@ -946,7 +946,7 @@ class SchemaManagerFunctionalTestCase extends \Doctrine\Tests\DbalFunctionalTest
 
     protected function getTestTable($name, $options=array())
     {
-        $table = new Table($name, array(), array(), array(), array(), false, $options);
+        $table = new Table($name, array(), array(), array(), array(), $options);
         $table->setSchemaConfig($this->_sm->createSchemaConfig());
         $table->addColumn('id', 'integer', array('notnull' => true));
         $table->setPrimaryKey(array('id'));
@@ -957,7 +957,7 @@ class SchemaManagerFunctionalTestCase extends \Doctrine\Tests\DbalFunctionalTest
 
     protected function getTestCompositeTable($name)
     {
-        $table = new Table($name, array(), array(), array(), array(), false, array());
+        $table = new Table($name, array(), array(), array(), array(), array());
         $table->setSchemaConfig($this->_sm->createSchemaConfig());
         $table->addColumn('id', 'integer', array('notnull' => true));
         $table->addColumn('other_id', 'integer', array('notnull' => true));
