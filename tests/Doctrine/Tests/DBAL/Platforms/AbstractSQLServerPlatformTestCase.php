708641b57a8b487e813b71c179562db520eda36d
Guilherme Blanco <guilhermeblanco@gmail.com>
2016-09-24 03:36:58 +0000

    Fixed tests

--- tests/Doctrine/Tests/DBAL/Platforms/AbstractSQLServerPlatformTestCase.php
+++ tests/Doctrine/Tests/DBAL/Platforms/AbstractSQLServerPlatformTestCase.php
@@ -1369,7 +1369,7 @@ abstract class AbstractSQLServerPlatformTestCase extends AbstractPlatformTestCas
      */
     protected function getQuotesReservedKeywordInUniqueConstraintDeclarationSQL()
     {
-        return 'CONSTRAINT [select] UNIQUE (foo) WHERE foo IS NOT NULL';
+        return 'CONSTRAINT [select] UNIQUE (foo)';
     }
 
     /**
