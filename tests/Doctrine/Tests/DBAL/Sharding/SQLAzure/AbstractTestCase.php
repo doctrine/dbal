bd4ea7f338a5b98475081a487062127ed78bf106
Steve Müller <st.mueller@dzh-online.de>
2017-02-05 19:06:16 +0100

    run SQL Azure related tests conditionally

--- tests/Doctrine/Tests/DBAL/Sharding/SQLAzure/AbstractTestCase.php
+++ tests/Doctrine/Tests/DBAL/Sharding/SQLAzure/AbstractTestCase.php
@@ -32,6 +32,13 @@ abstract class AbstractTestCase extends \PHPUnit_Framework_TestCase
             'driverOptions' => array('MultipleActiveResultSets' => false)
         );
         $this->conn = DriverManager::getConnection($params);
+
+        $serverEdition = $this->conn->fetchColumn("SELECT CONVERT(NVARCHAR(128), SERVERPROPERTY('Edition'))");
+
+        if (0 !== strpos($serverEdition, 'SQL Azure')) {
+            $this->markTestSkipped('SQL Azure only test.');
+        }
+
         // assume database is created and schema is:
         // Global products table
         // Customers, Orders, OrderItems federation tables.
