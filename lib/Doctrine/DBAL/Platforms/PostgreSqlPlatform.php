31db22630f925088a58a1d33bb4e401d3913f2d1
Chernyavtsev Ivan <chernyavtsev@intaro.ru>
2017-03-09 11:24:30 +0300

    Add slashes before '_' in postgresql like condition for system prefix 'pg_'

--- lib/Doctrine/DBAL/Platforms/PostgreSqlPlatform.php
+++ lib/Doctrine/DBAL/Platforms/PostgreSqlPlatform.php
@@ -237,7 +237,7 @@ class PostgreSqlPlatform extends AbstractPlatform
     {
         return "SELECT schema_name AS nspname
                 FROM   information_schema.schemata
-                WHERE  schema_name NOT LIKE 'pg_%'
+                WHERE  schema_name NOT LIKE 'pg\_%'
                 AND    schema_name != 'information_schema'";
     }
 
@@ -249,7 +249,7 @@ class PostgreSqlPlatform extends AbstractPlatform
         return "SELECT sequence_name AS relname,
                        sequence_schema AS schemaname
                 FROM   information_schema.sequences
-                WHERE  sequence_schema NOT LIKE 'pg_%'
+                WHERE  sequence_schema NOT LIKE 'pg\_%'
                 AND    sequence_schema != 'information_schema'";
     }
 
@@ -261,7 +261,7 @@ class PostgreSqlPlatform extends AbstractPlatform
         return "SELECT quote_ident(table_name) AS table_name,
                        table_schema AS schema_name
                 FROM   information_schema.tables
-                WHERE  table_schema NOT LIKE 'pg_%'
+                WHERE  table_schema NOT LIKE 'pg\_%'
                 AND    table_schema != 'information_schema'
                 AND    table_name != 'geometry_columns'
                 AND    table_name != 'spatial_ref_sys'
