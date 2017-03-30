84bbcbbb081375b820b0ff7cf44670eec23d1a7e
Jonathan Vollebregt <jnvsor@gmail.com>
2017-03-30 19:00:55 +0200

    MySqlPlatform: Fix default isolation level
    Default isolation level on mysql is REPEATABLE READ
    
    https://dev.mysql.com/doc/refman/5.7/en/server-system-variables.html#sysvar_tx_isolation

--- tests/Doctrine/Tests/DBAL/Platforms/MySqlPlatformTest.php
+++ tests/Doctrine/Tests/DBAL/Platforms/MySqlPlatformTest.php
@@ -3,11 +3,20 @@
 namespace Doctrine\Tests\DBAL\Platforms;
 
 use Doctrine\DBAL\Platforms\MySqlPlatform;
+use Doctrine\DBAL\Connection;
 
 class MySqlPlatformTest extends AbstractMySQLPlatformTestCase
 {
     public function createPlatform()
     {
         return new MysqlPlatform;
+    }
+
+    public function testHasCorrectDefaultTransactionIsolationLevel()
+    {
+        $this->assertEquals(
+            Connection::TRANSACTION_REPEATABLE_READ,
+            $this->_platform->getDefaultTransactionIsolationLevel()
+        );
     }
 }
