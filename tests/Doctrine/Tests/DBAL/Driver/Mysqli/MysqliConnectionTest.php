663cb65036c34d3c295ad2847ced65036aa497ab
Piotr Różański <piotr@develancer.pl>
2017-04-30 01:01:36 +0200

    Better exception handling in mysqli connect

--- tests/Doctrine/Tests/DBAL/Driver/Mysqli/MysqliConnectionTest.php
+++ tests/Doctrine/Tests/DBAL/Driver/Mysqli/MysqliConnectionTest.php
@@ -28,9 +28,31 @@ class MysqliConnectionTest extends DbalTestCase
             ->getMockForAbstractClass();
     }
 
+    protected function tearDown()
+    {
+        if ($this->getName() == "testRestoresErrorHandlerOnException") {
+            restore_error_handler();
+        }
+    }
+
     public function testDoesNotRequireQueryForServerVersion()
     {
         $this->assertFalse($this->connectionMock->requiresQueryForServerVersion());
+    }
+
+    public function testRestoresErrorHandlerOnException()
+    {
+        $handler = function () { self::fail('Never expected this to be called'); };
+        $default_handler = set_error_handler($handler);
+
+        try {
+            new MysqliConnection(['host' => '255.255.255.255'], 'user', 'pass');
+            self::fail('An exception was supposed to be raised');
+        } catch (MysqliException $e) {
+            self::assertSame('Network is unreachable', $e->getMessage());
+        }
+
+        self::assertSame($handler, set_error_handler($default_handler), 'Restoring error handler failed.');
     }
 
     /**
