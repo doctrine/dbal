79525423fa300fb613c7d74d8293af0b03ccb115
Marco Pivetta <ocramius@gmail.com>
2017-07-02 12:59:13 +0200

    [DBAL-2648] adding type hints to the `LastInsertId` class

--- lib/Doctrine/DBAL/Driver/LastInsertId.php
+++ lib/Doctrine/DBAL/Driver/LastInsertId.php
@@ -10,18 +10,12 @@ final class LastInsertId
     /** @var string */
     private $value = '0';
 
-    /**
-     * @return string
-     */
-    public function get()
+    public function get() : string
     {
         return $this->value;
     }
 
-    /**
-     * @param string $value
-     */
-    public function set($value)
+    public function set(string $value) : void
     {
         // The last insert ID is reset to "0" in certain situations by some implementations,
         // therefore we keep the previously set insert ID locally.
