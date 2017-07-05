1448b5d1b155469c54226d1585c7c742e16bd6b1
mondrake <mondrake@mondrake.org>
2017-07-05 11:11:43 +0200

    Temporarily removed changes, extended test method to include various db assets.

--- tests/Doctrine/Tests/DBAL/Functional/Schema/OracleSchemaManagerTest.php
+++ tests/Doctrine/Tests/DBAL/Functional/Schema/OracleSchemaManagerTest.php
@@ -276,19 +276,31 @@ class OracleSchemaManagerTest extends SchemaManagerFunctionalTestCase
 
     public function testCreateSchemaOnLargeNumberOfTables()
     {
+        // Create a base table for test.
         $sql = "CREATE TABLE tbl_test_2766_0 (x_id VARCHAR2(255) DEFAULT 'x' NOT NULL, x_data CLOB DEFAULT NULL NULL, x_number NUMBER(10) DEFAULT 0 NOT NULL, PRIMARY KEY(x_id))";
         $this->_conn->executeUpdate($sql);
 
-        for ($i = 1; $i < 150; $i++) {
-          $sql = "CREATE TABLE tbl_test_2766_$i (x_id VARCHAR2(255) DEFAULT 'x' NOT NULL, x_data CLOB DEFAULT NULL NULL, x_number NUMBER(10) DEFAULT 0 NOT NULL, PRIMARY KEY(x_id))";
+        // Create a large number of tables with indexes and foreign keys.
+        for ($i = 1; $i < 149; $i++) {
+          $sql = "CREATE TABLE tbl_test_2766_$i (x_id VARCHAR2(255) DEFAULT 'x' NOT NULL, x_data CLOB DEFAULT NULL NULL, x_number NUMBER(10) DEFAULT 0 NOT NULL, x_parent_id VARCHAR2(255) DEFAULT 'x' NOT NULL, FOREIGN KEY tbl_test_2766_fk_$i (x_parent_id) REFERENCES tbl_test_2766_0(x_id), PRIMARY KEY(x_id))";
+          $this->_conn->executeUpdate($sql);
+          $sql = "CREATE UNIQUE INDEX tbl_test_2766_uix_$i ON tbl_test_2766_$i (x_number)";
           $this->_conn->executeUpdate($sql);
         }
 
+        // Create a table with quoted identifiers.
+        $sql = "CREATE TABLE \"tbl_testq_2766_149\" (\"q_id\" VARCHAR2(255) DEFAULT 'x' NOT NULL, \"q_data\" CLOB DEFAULT NULL NULL, \"q_number\" NUMBER(10) DEFAULT 0 NOT NULL, \"q_parent_id\" VARCHAR2(255) DEFAULT 'x' NOT NULL, FOREIGN KEY \"tbl_testq_2766_fk_149\" (\"q_parent_id\") REFERENCES tbl_test_2766_0(x_id), PRIMARY KEY(\"q_id\"))";
+        $this->_conn->executeUpdate($sql);
+        $sql = "CREATE UNIQUE INDEX \"tbl_testq_2766_uix_149\" ON \"tbl_testq_2766_149\" (\"q_number\")";
+        $this->_conn->executeUpdate($sql);
+
+        // Introspect the db schema.
         $startTime = microtime(TRUE);
         $schema = $this->_sm->createSchema();
         $endTime = microtime(TRUE);
 
         $this->assertGreaterThanOrEqual(150, count($schema->getTables()));
+
         $this->assertLessThan(15, $endTime - $startTime, 'createSchema() executed in less than 15 sec.');
     }
 }
