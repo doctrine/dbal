9315e61c4c525167fbcc963c38ef136f691c99e9
Marco Pivetta <ocramius@gmail.com>
2017-07-22 22:45:23 +0200

    Bumping development version to `v2.5.14-DEV`

--- lib/Doctrine/DBAL/Version.php
+++ lib/Doctrine/DBAL/Version.php
@@ -34,7 +34,7 @@ class Version
     /**
      * Current Doctrine Version.
      */
-    const VERSION = '2.5.13';
+    const VERSION = '2.5.14-DEV';
 
     /**
      * Compares a Doctrine version with the current one.
