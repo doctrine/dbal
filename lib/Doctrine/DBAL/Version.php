498760e55195ccaf08076cb9100a2972ba74c001
Marco Pivetta <ocramius@gmail.com>
2017-07-23 03:02:22 +0200

    Preparing 2.6.0 release

--- lib/Doctrine/DBAL/Version.php
+++ lib/Doctrine/DBAL/Version.php
@@ -34,7 +34,7 @@ class Version
     /**
      * Current Doctrine Version.
      */
-    const VERSION = '2.6.0-DEV';
+    const VERSION = '2.6.0';
 
     /**
      * Compares a Doctrine version with the current one.
