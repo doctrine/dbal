0b32c9e7b0d43a806342b38aba5035c5a6a7b88e
Marco Pivetta <ocramius@gmail.com>
2017-08-28 13:08:35 +0200

    Bumping development release to 2.6.3-DEV

--- lib/Doctrine/DBAL/Version.php
+++ lib/Doctrine/DBAL/Version.php
@@ -34,7 +34,7 @@ class Version
     /**
      * Current Doctrine Version.
      */
-    const VERSION = '2.6.2';
+    const VERSION = '2.6.3-DEV';
 
     /**
      * Compares a Doctrine version with the current one.
