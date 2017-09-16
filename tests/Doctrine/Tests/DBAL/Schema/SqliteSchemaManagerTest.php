6f8f1e14f5de6bbd85c1e433615d6063b4d87c1d
Gawain Lynch <gawain.lynch@gmail.com>
2017-09-16 08:15:26 +0200

    [Tests] Check for correct collation when column names are similar

--- tests/Doctrine/Tests/DBAL/Schema/SqliteSchemaManagerTest.php
+++ tests/Doctrine/Tests/DBAL/Schema/SqliteSchemaManagerTest.php
@@ -2,6 +2,7 @@
 
 namespace Doctrine\Tests\DBAL\Schema;
 
+use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Platforms\SqlitePlatform;
 use Doctrine\DBAL\Schema\SqliteSchemaManager;
 
@@ -9,37 +10,32 @@ class SqliteSchemaManagerTest extends \PHPUnit\Framework\TestCase
 {
     /**
      * @dataProvider getDataColumnCollation
+     *
+     * @group 2865
      */
-    public function testParseColumnCollation($collation, $column, $sql)
+    public function testParseColumnCollation($collation, string $column, string $sql) : void
     {
-        $conn = $this->getMockBuilder('Doctrine\DBAL\Connection')->disableOriginalConstructor()->getMock();
-        $conn->expects($this->any())->method('getDatabasePlatform')->will($this->returnValue(new SqlitePlatform()));
+        $conn = $this->createMock(Connection::class);
+        $conn->method('getDatabasePlatform')->willReturn(new SqlitePlatform());
 
         $manager = new SqliteSchemaManager($conn);
-        $ref = new \ReflectionMethod($manager, 'parseColumnCollationFromSQL');
+        $ref     = new \ReflectionMethod($manager, 'parseColumnCollationFromSQL');
         $ref->setAccessible(true);
 
-        self::assertEquals($collation, $ref->invoke($manager, $column, $sql));
+        self::assertSame($collation, $ref->invoke($manager, $column, $sql));
     }
 
     public function getDataColumnCollation()
     {
-        return array(
-            array(
-                'RTRIM', 'a', 'CREATE TABLE "a" ("a" text DEFAULT "aa" COLLATE "RTRIM" NOT NULL)'
-            ),
-            array(
-                'utf-8', 'a', 'CREATE TABLE "a" ("b" text UNIQUE NOT NULL COLLATE NOCASE, "a" text DEFAULT "aa" COLLATE "utf-8" NOT NULL)'
-            ),
-            array(
-                'NOCASE', 'a', 'CREATE TABLE "a" ("a" text DEFAULT (lower(ltrim(" a") || rtrim("a "))) CHECK ("a") NOT NULL COLLATE NOCASE UNIQUE, "b" text COLLATE RTRIM)'
-            ),
-            array(
-                false, 'a', 'CREATE TABLE "a" ("a" text CHECK ("a") NOT NULL, "b" text COLLATE RTRIM)'
-            ),
-            array(
-                'RTRIM', 'a"b', 'CREATE TABLE "a" ("a""b" text COLLATE RTRIM)'
-            ),
-        );
+        return [
+            ['RTRIM', 'a', 'CREATE TABLE "a" ("a" text DEFAULT "aa" COLLATE "RTRIM" NOT NULL)'],
+            ['utf-8', 'a', 'CREATE TABLE "a" ("b" text UNIQUE NOT NULL COLLATE NOCASE, "a" text DEFAULT "aa" COLLATE "utf-8" NOT NULL)'],
+            ['NOCASE', 'a', 'CREATE TABLE "a" ("a" text DEFAULT (lower(ltrim(" a") || rtrim("a "))) CHECK ("a") NOT NULL COLLATE NOCASE UNIQUE, "b" text COLLATE RTRIM)'],
+            [false, 'a', 'CREATE TABLE "a" ("a" text CHECK ("a") NOT NULL, "b" text COLLATE RTRIM)'],
+            ['RTRIM', 'a"b', 'CREATE TABLE "a" ("a""b" text COLLATE RTRIM)'],
+            ['BINARY', 'b', 'CREATE TABLE "a" (bb TEXT COLLATE RTRIM, b VARCHAR(42) NOT NULL COLLATE BINARY)'],
+            ['BINARY', 'b', 'CREATE TABLE "a" (bbb TEXT COLLATE NOCASE, bb TEXT COLLATE RTRIM, b VARCHAR(42) NOT NULL COLLATE BINARY)'],
+            ['BINARY', 'b', 'CREATE TABLE "a" (b VARCHAR(42) NOT NULL COLLATE BINARY, bb TEXT COLLATE RTRIM)'],
+        ];
     }
 }
