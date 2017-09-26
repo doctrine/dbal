166405127ea9ae9a6bf7eaf53640e7d82148d0af
belgattitude <belgattitude@gmail.com>
2017-09-26 17:04:24 +0200

    Removed unused use statement

--- tests/Doctrine/Tests/DBAL/Functional/Schema/MySqlSchemaManagerTest.php
+++ tests/Doctrine/Tests/DBAL/Functional/Schema/MySqlSchemaManagerTest.php
@@ -7,7 +7,6 @@ use Doctrine\DBAL\Platforms\MySqlPlatform;
 use Doctrine\DBAL\Schema\Comparator;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\DBAL\Schema\Table;
-use Doctrine\DBAL\Schema\TableDiff;
 use Doctrine\DBAL\Types\Type;
 
 class MySqlSchemaManagerTest extends SchemaManagerFunctionalTestCase
