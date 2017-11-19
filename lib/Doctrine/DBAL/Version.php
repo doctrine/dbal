2202adf84e436c8cea7b9b529c3f3ac3ceb60591
Luís Cobucci <lcobucci@gmail.com>
2017-11-19 15:01:14 +0100

    Bumping development release to 2.6.4-DEV

--- lib/Doctrine/DBAL/Version.php
+++ lib/Doctrine/DBAL/Version.php
@@ -34,7 +34,7 @@ class Version
     /**
      * Current Doctrine Version.
      */
-    const VERSION = '2.6.3';
+    const VERSION = '2.6.4-DEV';
 
     /**
      * Compares a Doctrine version with the current one.
