72984f8c3d5493b04df5e9c9975b92d4b4f80a18
Dorian Savina <dorian.savina@gmail.com>
2017-12-20 02:02:44 +0100

    [major] SchemaDiff: specific foreign key renaming in generated SQL (Sqlite platform)
    Signed-off-by: Dorian Savina <dorian.savina@gmail.com>

--- lib/Doctrine/DBAL/Platforms/SqlitePlatform.php
+++ lib/Doctrine/DBAL/Platforms/SqlitePlatform.php
@@ -928,7 +928,7 @@ class SqlitePlatform extends AbstractPlatform
         if ( ! empty($diff->renamedColumns) || ! empty($diff->addedForeignKeys) || ! empty($diff->addedIndexes)
                 || ! empty($diff->changedColumns) || ! empty($diff->changedForeignKeys) || ! empty($diff->changedIndexes)
                 || ! empty($diff->removedColumns) || ! empty($diff->removedForeignKeys) || ! empty($diff->removedIndexes)
-                || ! empty($diff->renamedIndexes)
+                || ! empty($diff->renamedIndexes) || ! empty($diff->renamedForeignKeys)
         ) {
             return false;
         }
@@ -1078,6 +1078,12 @@ class SqlitePlatform extends AbstractPlatform
         $columnNames = $this->getColumnNamesInAlteredTable($diff);
 
         foreach ($foreignKeys as $key => $constraint) {
+            foreach ($diff->renamedForeignKeys as $oldForeignKeyName => $renamedForeignKey) {
+                if (strcasecmp($key, $oldForeignKeyName) === 0) {
+                    unset($foreignKeys[$key]);
+                }
+            }
+
             $changed = false;
             $localColumns = [];
             foreach ($constraint->getLocalColumns() as $columnName) {
@@ -1105,7 +1111,7 @@ class SqlitePlatform extends AbstractPlatform
             }
         }
 
-        foreach (array_merge($diff->changedForeignKeys, $diff->addedForeignKeys) as $constraint) {
+        foreach (array_merge($diff->changedForeignKeys, $diff->addedForeignKeys, $diff->renamedForeignKeys) as $constraint) {
             $constraintName = strtolower($constraint->getName());
             if (strlen($constraintName)) {
                 $foreignKeys[$constraintName] = $constraint;
