14082e4e1cb6cd2e524b4c4e20a7817aa771adcc
Sergei Morozov <morozov@tut.by>
2017-12-31 19:25:28 -0800

    [BC] Replaced PDO constants with values

--- lib/Doctrine/DBAL/ColumnCase.php
+++ lib/Doctrine/DBAL/ColumnCase.php
@@ -12,14 +12,14 @@ final class ColumnCase
      *
      * @see \PDO::CASE_UPPER
      */
-    public const UPPER = \PDO::CASE_UPPER;
+    public const UPPER = 1;
 
     /**
      * Convert column names to lower case.
      *
      * @see \PDO::CASE_LOWER
      */
-    public const LOWER = \PDO::CASE_LOWER;
+    public const LOWER = 2;
 
     /**
      * This class cannot be instantiated.
