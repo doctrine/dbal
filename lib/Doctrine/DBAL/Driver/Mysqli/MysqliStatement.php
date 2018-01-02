00f5c2220075009460ec73d899f38113a9c6b25b
Sergei Morozov <morozov@tut.by>
2018-01-02 15:07:37 -0800

    [BC] Replaced extension of \PDOStatement with a composition to avoid having to replicate the \PDOStatement interface in ResultStatement

--- lib/Doctrine/DBAL/Driver/Mysqli/MysqliStatement.php
+++ lib/Doctrine/DBAL/Driver/Mysqli/MysqliStatement.php
@@ -256,7 +256,7 @@ class MysqliStatement implements \IteratorAggregate, Statement
     /**
      * {@inheritdoc}
      */
-    public function fetch($fetchMode = null, $cursorOrientation = \PDO::FETCH_ORI_NEXT, $cursorOffset = 0)
+    public function fetch($fetchMode = null, ...$args)
     {
         // do not try fetching from the statement if it's not expected to contain result
         // in order to prevent exceptional situation
@@ -310,7 +310,7 @@ class MysqliStatement implements \IteratorAggregate, Statement
     /**
      * {@inheritdoc}
      */
-    public function fetchAll($fetchMode = null, $fetchArgument = null, $ctorArgs = null)
+    public function fetchAll($fetchMode = null, ...$args)
     {
         $fetchMode = $fetchMode ?: $this->_defaultFetchMode;
 
@@ -393,7 +393,7 @@ class MysqliStatement implements \IteratorAggregate, Statement
     /**
      * {@inheritdoc}
      */
-    public function setFetchMode($fetchMode, $arg2 = null, $arg3 = null)
+    public function setFetchMode($fetchMode, ...$args)
     {
         $this->_defaultFetchMode = $fetchMode;
 
