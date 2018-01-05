94ba5640d5e49454916e45376ead9b81f266fbb6
Michael Moravec <mail@majkl578.cz>
2018-01-05 05:16:58 +0100

    Bump version to 3.0.x-dev

--- lib/Doctrine/DBAL/Version.php
+++ lib/Doctrine/DBAL/Version.php
@@ -21,7 +21,7 @@ class Version
     /**
      * Current Doctrine Version.
      */
-    public const VERSION = '2.8.0-DEV';
+    const VERSION = '3.0.0-DEV';
 
     /**
      * Compares a Doctrine version with the current one.
