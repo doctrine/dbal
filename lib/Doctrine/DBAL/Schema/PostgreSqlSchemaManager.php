638353a05e5c530843d121188126a6fcbc67ec3d
josephzhao <zhaoshuilin2004@yahoo.ca>
2018-01-11 16:13:58 -0500

    Update PostgreSqlSchemaManager.php

--- lib/Doctrine/DBAL/Schema/PostgreSqlSchemaManager.php
+++ lib/Doctrine/DBAL/Schema/PostgreSqlSchemaManager.php
@@ -289,9 +289,18 @@ class PostgreSqlSchemaManager extends AbstractSchemaManager
             $sequenceName = $sequence['relname'];
         }
 
-        $data = $this->_conn->fetchAll('SELECT min_value, increment_by FROM ' . $this->_platform->quoteIdentifier($sequenceName));
+       $version = floatval($this->_conn->getWrappedConnection()->getServerVersion());
 
-        return new Sequence($sequenceName, $data[0]['increment_by'], $data[0]['min_value']);
+        if ($version >= 10) {
+            $data = $this->_conn->fetchAll('SELECT min_value, increment_by FROM pg_sequences WHERE schemaname = \'public\' AND sequencename = ' . $this->_conn->quote($sequenceName));
+        } else {
+            $data = $this->_conn->fetchAll('SELECT min_value, increment_by FROM ' . $this->_platform->quoteIdentifier($sequenceName));
+        }
+        if (count($data) > 0) {
+            return new Sequence($sequenceName, $data[0]['increment_by'], $data[0]['min_value']);
+        } else {
+            return new Sequence($sequenceName, 1, 1);
+        }
     }
 
     /**
