de2bfb1210a2614cd55468db8fea3851d946ed4e
Grégoire Paris <gparis@universcine.com>
2018-02-08 15:26:42 +0100

    Provide FROM clause to Oracle
    See
    https://stackoverflow.com/questions/1881853/select-without-a-from-clause-in-oracle

--- tests/Doctrine/Tests/DBAL/Functional/LikeWildcardsEscapingTest.php
+++ tests/Doctrine/Tests/DBAL/Functional/LikeWildcardsEscapingTest.php
@@ -2,18 +2,21 @@
 
 namespace Doctrine\Tests\DBAL\Functional;
 
+use Doctrine\DBAL\Platforms\OraclePlatform;
 use Doctrine\Tests\DbalFunctionalTestCase;
 
 final class LikeWildcardsEscapingTest extends DbalFunctionalTestCase
 {
     public function testFetchLikeExpressionResult() : void
     {
-        $string     = '_25% off_ your next purchase \o/';
-        $escapeChar = '!';
-        $stmt       = $this->_conn->prepare(sprintf(
-            "SELECT '%s' LIKE '%s' ESCAPE '%s' as it_matches",
+        $string           = '_25% off_ your next purchase \o/';
+        $escapeChar       = '!';
+        $databasePlatform = $this->_conn->getDatabasePlatform();
+        $stmt             = $this->_conn->prepare(sprintf(
+            "SELECT (CASE WHEN '%s' LIKE '%s' ESCAPE '%s' THEN 1 ELSE 0 END)" .
+                ($databasePlatform instanceof OraclePlatform ? ' FROM dual' : ''),
             $string,
-            $this->_conn->getDatabasePlatform()->escapeStringForLike($string, $escapeChar),
+            $databasePlatform->escapeStringForLike($string, $escapeChar),
             $escapeChar
         ));
         $stmt->execute();
