44f7c1396230da90984d481e7cb71704fbc894cb
Grégoire Paris <postmaster@greg0ire.fr>
2018-02-09 20:51:23 +0100

    Document the new parameter

--- lib/Doctrine/DBAL/Query/Expression/ExpressionBuilder.php
+++ lib/Doctrine/DBAL/Query/Expression/ExpressionBuilder.php
@@ -257,7 +257,7 @@ class ExpressionBuilder
      *
      * @return string
      */
-    public function like($x, $y)
+    public function like($x, $y/*, ?string $escapeChar = null */)
     {
         return $this->comparison($x, 'LIKE', $y) .
             (func_num_args() >= 3 ? sprintf(" ESCAPE '%s'", func_get_arg(2)) : '');
@@ -271,7 +271,7 @@ class ExpressionBuilder
      *
      * @return string
      */
-    public function notLike($x, $y)
+    public function notLike($x, $y/*, ?string $escapeChar = null */)
     {
         return $this->comparison($x, 'NOT LIKE', $y) .
             (func_num_args() >= 3 ? sprintf(" ESCAPE '%s'", func_get_arg(2)) : '');
