fe7d9ad9285abb5d059b993362a905dfa715b715
Benjamin Morel <benjamin.morel@gmail.com>
2018-03-08 19:40:42 +0100

    Change the SQLLogger interface signature

--- lib/Doctrine/DBAL/Logging/SQLLogger.php
+++ lib/Doctrine/DBAL/Logging/SQLLogger.php
@@ -17,18 +17,18 @@ interface SQLLogger
     /**
      * Logs a SQL statement somewhere.
      *
-     * @param string     $sql    The SQL to be executed.
-     * @param array|null $params The SQL parameters.
-     * @param array|null $types  The SQL parameter types.
+     * @param string $sql    The SQL to be executed.
+     * @param array  $params The SQL parameters.
+     * @param array  $types  The SQL parameter types.
      *
      * @return void
      */
-    public function startQuery($sql, array $params = null, array $types = null);
+    public function startQuery(string $sql, array $params = [], array $types = []) : void;
 
     /**
      * Marks the last started query as stopped. This can be used for timing of queries.
      *
      * @return void
      */
-    public function stopQuery();
+    public function stopQuery() : void;
 }
