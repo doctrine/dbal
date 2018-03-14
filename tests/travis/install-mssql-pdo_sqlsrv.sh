150a55b74bf3e8f61b209ca0e7f4b4d3d594359f
Michael Moravec <me@majkl.me>
2018-03-15 00:42:55 +0100

    Travis: Test MS SQL Server 17

---
+++ tests/travis/install-mssql-pdo_sqlsrv.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -ex
+
+echo "Installing extension"
+pecl install pdo_sqlsrv-5.2.0RC1
---
+++ tests/travis/install-mssql-sqlsrv.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -ex
+
+echo "Installing extension"
+pecl install sqlsrv-5.2.0RC1
