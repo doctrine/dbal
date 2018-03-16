008528032f8fa982e2f0c68218cbd4fd14c3f886
Michael Moravec <me@majkl.me>
2018-03-16 18:30:16 +0100

    Drop support for PostgreSQL <9.3

--- tests/Doctrine/Tests/DBAL/Platforms/PostgreSqlPlatformTest.php
+++ tests/Doctrine/Tests/DBAL/Platforms/PostgreSqlPlatformTest.php
@@ -3,6 +3,7 @@
 namespace Doctrine\Tests\DBAL\Platforms;
 
 use Doctrine\DBAL\Platforms\PostgreSqlPlatform;
+use Doctrine\DBAL\Types\Type;
 
 class PostgreSqlPlatformTest extends AbstractPostgreSqlPlatformTestCase
 {
@@ -14,5 +15,67 @@ class PostgreSqlPlatformTest extends AbstractPostgreSqlPlatformTestCase
     public function testSupportsPartialIndexes()
     {
         self::assertTrue($this->platform->supportsPartialIndexes());
+    }
+
+    public function testColumnCollationDeclarationSQL()
+    {
+        self::assertEquals(
+            'COLLATE "en_US.UTF-8"',
+            $this->platform->getColumnCollationDeclarationSQL('en_US.UTF-8')
+        );
+    }
+
+    /**
+     * @group DBAL-553
+     */
+    public function testHasNativeJsonType()
+    {
+        self::assertTrue($this->platform->hasNativeJsonType());
+    }
+
+    /**
+     * @group DBAL-553
+     */
+    public function testReturnsJsonTypeDeclarationSQL()
+    {
+        self::assertSame('JSON', $this->platform->getJsonTypeDeclarationSQL([]));
+    }
+
+    public function testReturnsSmallIntTypeDeclarationSQL()
+    {
+        self::assertSame(
+            'SMALLSERIAL',
+            $this->platform->getSmallIntTypeDeclarationSQL(['autoincrement' => true])
+        );
+
+        self::assertSame(
+            'SMALLINT',
+            $this->platform->getSmallIntTypeDeclarationSQL(['autoincrement' => false])
+        );
+
+        self::assertSame(
+            'SMALLINT',
+            $this->platform->getSmallIntTypeDeclarationSQL([])
+        );
+    }
+
+    /**
+     * @group DBAL-553
+     */
+    public function testInitializesJsonTypeMapping()
+    {
+        self::assertTrue($this->platform->hasDoctrineTypeMappingFor('json'));
+        self::assertEquals(Type::JSON, $this->platform->getDoctrineTypeMapping('json'));
+    }
+
+    /**
+     * @group DBAL-1220
+     */
+    public function testReturnsCloseActiveDatabaseConnectionsSQL()
+    {
+        self::assertSame(
+            "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'foo'",
+            $this->platform->getCloseActiveDatabaseConnectionsSQL('foo')
+        );
     }
 }
