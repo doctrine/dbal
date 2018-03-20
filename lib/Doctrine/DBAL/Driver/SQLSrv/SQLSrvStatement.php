doctrine/dbal

commit 2b8c40d267b4ac699ea878dcca07332e1697978c

[BC] Replaced extension of \PDOStatement with a composition to avoid having to replicate the \PDOStatement interface in ResultStatement

lib/Doctrine/DBAL/Driver/SQLSrv/SQLSrvStatement.php
@@ -267,11 +267,17 @@ class SQLSrvStatement implements IteratorAggregate, Statement
     /**
      * {@inheritdoc}
      */
-    public function setFetchMode($fetchMode, $arg2 = null, $arg3 = null)
+    public function setFetchMode($fetchMode, ...$args)
     {
-        $this->defaultFetchMode          = $fetchMode;
-        $this->defaultFetchClass         = $arg2 ?: $this->defaultFetchClass;
-        $this->defaultFetchClassCtorArgs = $arg3 ? (array) $arg3 : $this->defaultFetchClassCtorArgs;
+        $this->defaultFetchMode = $fetchMode;
+
+        if (isset($args[0])) {
+            $this->defaultFetchClass = $args[0];
+        }
+
+        if (isset($args[1])) {
+            $this->defaultFetchClassCtorArgs = (array) $args[2];
+        }
 
         return true;
     }
@@ -289,7 +295,7 @@ class SQLSrvStatement implements IteratorAggregate, Statement
      *
      * @throws SQLSrvException
      */
-    public function fetch($fetchMode = null, $cursorOrientation = \PDO::FETCH_ORI_NEXT, $cursorOffset = 0)
+    public function fetch($fetchMode = null, ...$args)
     {
         // do not try fetching from the statement if it's not expected to contain result
         // in order to prevent exceptional situation
@@ -297,7 +303,6 @@ class SQLSrvStatement implements IteratorAggregate, Statement
             return false;
         }
 
-        $args      = func_get_args();
         $fetchMode = $fetchMode ?: $this->defaultFetchMode;
 
         if ($fetchMode === FetchMode::COLUMN) {
@@ -312,9 +317,9 @@ class SQLSrvStatement implements IteratorAggregate, Statement
             $className = $this->defaultFetchClass;
             $ctorArgs  = $this->defaultFetchClassCtorArgs;
 
-            if (count($args) >= 2) {
-                $className = $args[1];
-                $ctorArgs  = $args[2] ?? [];
+            if (count($args) > 0) {
+                $className = $args[0];
+                $ctorArgs  = $args[1] ?? [];
             }
 
             return sqlsrv_fetch_object($this->stmt, $className, $ctorArgs) ?: false;
@@ -326,13 +331,13 @@ class SQLSrvStatement implements IteratorAggregate, Statement
     /**
      * {@inheritdoc}
      */
-    public function fetchAll($fetchMode = null, $fetchArgument = null, $ctorArgs = null)
+    public function fetchAll($fetchMode = null, ...$args)
     {
         $rows = [];
 
         switch ($fetchMode) {
             case FetchMode::CUSTOM_OBJECT:
-                while (($row = $this->fetch(...func_get_args())) !== false) {
+                while (($row = $this->fetch($fetchMode, $args)) !== false) {
                     $rows[] = $row;
                 }
                 break;

lib/Doctrine/DBAL/Cache/ArrayStatement.php
@@ -57,9 +57,9 @@ class ArrayStatement implements \IteratorAggregate, ResultStatement
     /**
      * {@inheritdoc}
      */
-    public function setFetchMode($fetchMode, $arg2 = null, $arg3 = null)
+    public function setFetchMode($fetchMode, ...$args)
     {
-        if ($arg2 !== null || $arg3 !== null) {
+        if (count($args) > 0) {
             throw new \InvalidArgumentException("Caching layer does not support 2nd/3rd argument to setFetchMode()");
         }
 
@@ -81,7 +81,7 @@ class ArrayStatement implements \IteratorAggregate, ResultStatement
     /**
      * {@inheritdoc}
      */
-    public function fetch($fetchMode = null, $cursorOrientation = \PDO::FETCH_ORI_NEXT, $cursorOffset = 0)
+    public function fetch($fetchMode = null, ...$args)
     {
         if (! isset($this->data[$this->num])) {
             return false;
@@ -112,10 +112,10 @@ class ArrayStatement implements \IteratorAggregate, ResultStatement
     /**
      * {@inheritdoc}
      */
-    public function fetchAll($fetchMode = null, $fetchArgument = null, $ctorArgs = null)
+    public function fetchAll($fetchMode = null, ...$args)
     {
         $rows = [];
-        while ($row = $this->fetch($fetchMode)) {
+        while ($row = $this->fetch($fetchMode, ...$args)) {
             $rows[] = $row;
         }
 

lib/Doctrine/DBAL/Cache/ResultCacheStatement.php
@@ -110,7 +110,7 @@ class ResultCacheStatement implements \IteratorAggregate, ResultStatement
     /**
      * {@inheritdoc}
      */
-    public function setFetchMode($fetchMode, $arg2 = null, $arg3 = null)
+    public function setFetchMode($fetchMode, ...$args)
     {
         $this->defaultFetchMode = $fetchMode;
 
@@ -130,7 +130,7 @@ class ResultCacheStatement implements \IteratorAggregate, ResultStatement
     /**
      * {@inheritdoc}
      */
-    public function fetch($fetchMode = null, $cursorOrientation = \PDO::FETCH_ORI_NEXT, $cursorOffset = 0)
+    public function fetch($fetchMode = null, ...$args)
     {
         if ($this->data === null) {
             $this->data = [];
@@ -170,10 +170,10 @@ class ResultCacheStatement implements \IteratorAggregate, ResultStatement
     /**
      * {@inheritdoc}
      */
-    public function fetchAll($fetchMode = null, $fetchArgument = null, $ctorArgs = null)
+    public function fetchAll($fetchMode = null, ...$args)
     {
         $rows = [];
-        while ($row = $this->fetch($fetchMode)) {
+        while ($row = $this->fetch($fetchMode, ...$args)) {
             $rows[] = $row;
         }
 

lib/Doctrine/DBAL/Driver/IBMDB2/DB2Statement.php
@@ -171,11 +171,17 @@ class DB2Statement implements \IteratorAggregate, Statement
     /**
      * {@inheritdoc}
      */
-    public function setFetchMode($fetchMode, $arg2 = null, $arg3 = null)
+    public function setFetchMode($fetchMode, ...$args)
     {
-        $this->_defaultFetchMode         = $fetchMode;
-        $this->defaultFetchClass         = $arg2 ? $arg2 : $this->defaultFetchClass;
-        $this->defaultFetchClassCtorArgs = $arg3 ? (array) $arg3 : $this->defaultFetchClassCtorArgs;
+        $this->_defaultFetchMode = $fetchMode;
+
+        if (isset($args[0])) {
+            $this->defaultFetchClass = $args[0];
+        }
+
+        if (isset($args[1])) {
+            $this->defaultFetchClassCtorArgs = (array) $args[2];
+        }
 
         return true;
     }
@@ -191,7 +197,7 @@ class DB2Statement implements \IteratorAggregate, Statement
     /**
      * {@inheritdoc}
      */
-    public function fetch($fetchMode = null, $cursorOrientation = \PDO::FETCH_ORI_NEXT, $cursorOffset = 0)
+    public function fetch($fetchMode = null, ...$args)
     {
         // do not try fetching from the statement if it's not expected to contain result
         // in order to prevent exceptional situation
@@ -214,10 +220,9 @@ class DB2Statement implements \IteratorAggregate, Statement
                 $className = $this->defaultFetchClass;
                 $ctorArgs  = $this->defaultFetchClassCtorArgs;
 
-                if (func_num_args() >= 2) {
-                    $args      = func_get_args();
-                    $className = $args[1];
-                    $ctorArgs  = $args[2] ?? [];
+                if (count($args) > 0) {
+                    $className = $args[0];
+                    $ctorArgs  = $args[1] ?? [];
                 }
 
                 $result = db2_fetch_object($this->_stmt);
@@ -242,23 +247,23 @@ class DB2Statement implements \IteratorAggregate, Statement
     /**
      * {@inheritdoc}
      */
-    public function fetchAll($fetchMode = null, $fetchArgument = null, $ctorArgs = null)
+    public function fetchAll($fetchMode = null, ...$args)
     {
         $rows = [];
 
         switch ($fetchMode) {
             case FetchMode::CUSTOM_OBJECT:
-                while ($row = call_user_func_array([$this, 'fetch'], func_get_args())) {
+                while (($row = $this->fetch($fetchMode, ...$args)) !== false) {
                     $rows[] = $row;
                 }
                 break;
             case FetchMode::COLUMN:
-                while ($row = $this->fetchColumn()) {
+                while (($row = $this->fetchColumn()) !== false) {
                     $rows[] = $row;
                 }
                 break;
             default:
-                while ($row = $this->fetch($fetchMode)) {
+                while (($row = $this->fetch($fetchMode)) !== false) {
                     $rows[] = $row;
                 }
         }

lib/Doctrine/DBAL/Driver/Mysqli/MysqliStatement.php
@@ -250,7 +250,7 @@ class MysqliStatement implements \IteratorAggregate, Statement
     /**
      * {@inheritdoc}
      */
-    public function fetch($fetchMode = null, $cursorOrientation = \PDO::FETCH_ORI_NEXT, $cursorOffset = 0)
+    public function fetch($fetchMode = null, ...$args)
     {
         // do not try fetching from the statement if it's not expected to contain result
         // in order to prevent exceptional situation
@@ -304,7 +304,7 @@ class MysqliStatement implements \IteratorAggregate, Statement
     /**
      * {@inheritdoc}
      */
-    public function fetchAll($fetchMode = null, $fetchArgument = null, $ctorArgs = null)
+    public function fetchAll($fetchMode = null, ...$args)
     {
         $fetchMode = $fetchMode ?: $this->_defaultFetchMode;
 
@@ -387,7 +387,7 @@ class MysqliStatement implements \IteratorAggregate, Statement
     /**
      * {@inheritdoc}
      */
-    public function setFetchMode($fetchMode, $arg2 = null, $arg3 = null)
+    public function setFetchMode($fetchMode, ...$args)
     {
         $this->_defaultFetchMode = $fetchMode;
 

lib/Doctrine/DBAL/Driver/OCI8/OCI8Statement.php
@@ -343,7 +343,7 @@ class OCI8Statement implements IteratorAggregate, Statement
     /**
      * {@inheritdoc}
      */
-    public function setFetchMode($fetchMode, $arg2 = null, $arg3 = null)
+    public function setFetchMode($fetchMode, ...$args)
     {
         $this->_defaultFetchMode = $fetchMode;
 
@@ -361,7 +361,7 @@ class OCI8Statement implements IteratorAggregate, Statement
     /**
      * {@inheritdoc}
      */
-    public function fetch($fetchMode = null, $cursorOrientation = \PDO::FETCH_ORI_NEXT, $cursorOffset = 0)
+    public function fetch($fetchMode = null, ...$args)
     {
         // do not try fetching from the statement if it's not expected to contain result
         // in order to prevent exceptional situation
@@ -392,7 +392,7 @@ class OCI8Statement implements IteratorAggregate, Statement
     /**
      * {@inheritdoc}
      */
-    public function fetchAll($fetchMode = null, $fetchArgument = null, $ctorArgs = null)
+    public function fetchAll($fetchMode = null, ...$args)
     {
         $fetchMode = $fetchMode ?: $this->_defaultFetchMode;
 

lib/Doctrine/DBAL/Driver/PDOConnection.php
@@ -25,7 +25,6 @@ class PDOConnection extends PDO implements Connection, ServerInfoAwareConnection
     {
         try {
             parent::__construct($dsn, $user, $password, $options);
-            $this->setAttribute(PDO::ATTR_STATEMENT_CLASS, ['Doctrine\DBAL\Driver\PDOStatement', []]);
             $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (\PDOException $exception) {
             throw new PDOException($exception);
@@ -58,7 +57,9 @@ class PDOConnection extends PDO implements Connection, ServerInfoAwareConnection
     public function prepare($prepareString, $driverOptions = [])
     {
         try {
-            return parent::prepare($prepareString, $driverOptions);
+            return $this->createStatement(
+                parent::prepare($prepareString, $driverOptions)
+            );
         } catch (\PDOException $exception) {
             throw new PDOException($exception);
         }
@@ -70,22 +71,11 @@ class PDOConnection extends PDO implements Connection, ServerInfoAwareConnection
     public function query()
     {
         $args = func_get_args();
-        $argsCount = count($args);
 
         try {
-            if ($argsCount == 4) {
-                return parent::query($args[0], $args[1], $args[2], $args[3]);
-            }
-
-            if ($argsCount == 3) {
-                return parent::query($args[0], $args[1], $args[2]);
-            }
-
-            if ($argsCount == 2) {
-                return parent::query($args[0], $args[1]);
-            }
-
-            return parent::query($args[0]);
+            return $this->createStatement(
+                parent::query(...$args)
+            );
         } catch (\PDOException $exception) {
             throw new PDOException($exception);
         }
@@ -113,5 +103,16 @@ class PDOConnection extends PDO implements Connection, ServerInfoAwareConnection
     public function requiresQueryForServerVersion()
     {
         return false;
+    }
+
+    /**
+     * Creates a wrapped statement
+     *
+     * @param \PDOStatement $stmt
+     * @return PDOStatement
+     */
+    private function createStatement(\PDOStatement $stmt) : PDOStatement
+    {
+        return new PDOStatement($stmt);
     }
 }

lib/Doctrine/DBAL/Driver/PDOStatement.php
@@ -4,6 +4,7 @@ namespace Doctrine\DBAL\Driver;
 
 use Doctrine\DBAL\FetchMode;
 use Doctrine\DBAL\ParameterType;
+use IteratorAggregate;
 use PDO;
 
 /**
@@ -12,7 +13,7 @@ use PDO;
  *
  * @since 2.0
  */
-class PDOStatement extends \PDOStatement implements Statement
+class PDOStatement implements IteratorAggregate, Statement
 {
     /**
      * @var int[]
@@ -38,33 +39,24 @@ class PDOStatement extends \PDOStatement implements Statement
     ];
 
     /**
-     * Protected constructor.
+     * @var \PDOStatement
      */
-    protected function __construct()
+    private $stmt;
+
+    public function __construct(\PDOStatement $stmt)
     {
+        $this->stmt = $stmt;
     }
 
     /**
      * {@inheritdoc}
      */
-    public function setFetchMode($fetchMode, $arg2 = null, $arg3 = null)
+    public function setFetchMode($fetchMode, ...$args)
     {
         $fetchMode = $this->convertFetchMode($fetchMode);
 
-        // This thin wrapper is necessary to shield against the weird signature
-        // of PDOStatement::setFetchMode(): even if the second and third
-        // parameters are optional, PHP will not let us remove it from this
-        // declaration.
         try {
-            if ($arg2 === null && $arg3 === null) {
-                return parent::setFetchMode($fetchMode);
-            }
-
-            if ($arg3 === null) {
-                return parent::setFetchMode($fetchMode, $arg2);
-            }
-
-            return parent::setFetchMode($fetchMode, $arg2, $arg3);
+            return $this->stmt->setFetchMode($fetchMode, ...$args);
         } catch (\PDOException $exception) {
             throw new PDOException($exception);
         }
@@ -78,7 +70,7 @@ class PDOStatement extends \PDOStatement implements Statement
         $type = $this->convertParamType($type);
 
         try {
-            return parent::bindValue($param, $value, $type);
+            return $this->stmt->bindValue($param, $value, $type);
         } catch (\PDOException $exception) {
             throw new PDOException($exception);
         }
@@ -92,7 +84,7 @@ class PDOStatement extends \PDOStatement implements Statement
         $type = $this->convertParamType($type);
 
         try {
-            return parent::bindParam($column, $variable, $type, $length, $driverOptions);
+            return $this->stmt->bindParam($column, $variable, $type, $length, $driverOptions);
         } catch (\PDOException $exception) {
             throw new PDOException($exception);
         }
@@ -104,7 +96,7 @@ class PDOStatement extends \PDOStatement implements Statement
     public function closeCursor()
     {
         try {
-            return parent::closeCursor();
+            return $this->stmt->closeCursor();
         } catch (\PDOException $exception) {
             // Exceptions not allowed by the interface.
             // In case driver implementations do not adhere to the interface, silence exceptions here.
@@ -115,10 +107,34 @@ class PDOStatement extends \PDOStatement implements Statement
     /**
      * {@inheritdoc}
      */
+    public function columnCount()
+    {
+        return $this->stmt->columnCount();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function errorCode()
+    {
+        return $this->stmt->errorCode();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function errorInfo()
+    {
+        return $this->stmt->errorInfo();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function execute($params = null)
     {
         try {
-            return parent::execute($params);
+            return $this->stmt->execute($params);
         } catch (\PDOException $exception) {
             throw new PDOException($exception);
         }
@@ -127,24 +143,24 @@ class PDOStatement extends \PDOStatement implements Statement
     /**
      * {@inheritdoc}
      */
-    public function fetch($fetchMode = null, $cursorOrientation = \PDO::FETCH_ORI_NEXT, $cursorOffset = 0)
+    public function rowCount()
+    {
+        return $this->stmt->rowCount();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function fetch($fetchMode = null, ...$args)
     {
         $fetchMode = $this->convertFetchMode($fetchMode);
 
         try {
-            if ($fetchMode === null && \PDO::FETCH_ORI_NEXT === $cursorOrientation && 0 === $cursorOffset) {
-                return parent::fetch();
+            if ($fetchMode === null) {
+                return $this->stmt->fetch();
             }
 
-            if (\PDO::FETCH_ORI_NEXT === $cursorOrientation && 0 === $cursorOffset) {
-                return parent::fetch($fetchMode);
-            }
-
-            if (0 === $cursorOffset) {
-                return parent::fetch($fetchMode, $cursorOrientation);
-            }
-
-            return parent::fetch($fetchMode, $cursorOrientation, $cursorOffset);
+            return $this->stmt->fetch($fetchMode, ...$args);
         } catch (\PDOException $exception) {
             throw new PDOException($exception);
         }
@@ -153,24 +169,16 @@ class PDOStatement extends \PDOStatement implements Statement
     /**
      * {@inheritdoc}
      */
-    public function fetchAll($fetchMode = null, $fetchArgument = null, $ctorArgs = null)
+    public function fetchAll($fetchMode = null, ...$args)
     {
         $fetchMode = $this->convertFetchMode($fetchMode);
 
         try {
-            if ($fetchMode === null && null === $fetchArgument && null === $ctorArgs) {
-                return parent::fetchAll();
+            if ($fetchMode === null) {
+                return $this->stmt->fetchAll();
             }
 
-            if (null === $fetchArgument && null === $ctorArgs) {
-                return parent::fetchAll($fetchMode);
-            }
-
-            if (null === $ctorArgs) {
-                return parent::fetchAll($fetchMode, $fetchArgument);
-            }
-
-            return parent::fetchAll($fetchMode, $fetchArgument, $ctorArgs);
+            return $this->stmt->fetchAll($fetchMode, ...$args);
         } catch (\PDOException $exception) {
             throw new PDOException($exception);
         }
@@ -182,7 +190,7 @@ class PDOStatement extends \PDOStatement implements Statement
     public function fetchColumn($columnIndex = 0)
     {
         try {
-            return parent::fetchColumn($columnIndex);
+            return $this->stmt->fetchColumn($columnIndex);
         } catch (\PDOException $exception) {
             throw new PDOException($exception);
         }
@@ -218,5 +226,13 @@ class PDOStatement extends \PDOStatement implements Statement
         }
 
         return self::FETCH_MODE_MAP[$fetchMode];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getIterator()
+    {
+        yield from $this->stmt;
     }
 }

commit e27622bbefd121735aad36f223c9fd6574037894

Fixed build failure on SQL Server (sqlsrv)
The regression was introduced by 2b8c40d267b4ac699ea878dcca07332e1697978c.

Fixes #3060.

lib/Doctrine/DBAL/Driver/SQLSrv/SQLSrvStatement.php
@@ -276,7 +276,7 @@ class SQLSrvStatement implements IteratorAggregate, Statement
         }
 
         if (isset($args[1])) {
-            $this->defaultFetchClassCtorArgs = (array) $args[2];
+            $this->defaultFetchClassCtorArgs = (array) $args[1];
         }
 
         return true;
@@ -337,7 +337,7 @@ class SQLSrvStatement implements IteratorAggregate, Statement
 
         switch ($fetchMode) {
             case FetchMode::CUSTOM_OBJECT:
-                while (($row = $this->fetch($fetchMode, $args)) !== false) {
+                while (($row = $this->fetch($fetchMode, ...$args)) !== false) {
                     $rows[] = $row;
                 }
                 break;