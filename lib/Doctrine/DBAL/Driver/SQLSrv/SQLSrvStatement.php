0949d5f7c14942567fb01495d8e26f872a6446c4
Sergei Morozov <morozov@tut.by>
2018-03-19 19:59:11 -0700

    Fixed build failure on SQL Server (sqlsrv)
    The regression was introduced by 2b8c40d267b4ac699ea878dcca07332e1697978c.
    
    Fixes #3060.

--- lib/Doctrine/DBAL/Driver/SQLSrv/SQLSrvStatement.php
+++ lib/Doctrine/DBAL/Driver/SQLSrv/SQLSrvStatement.php
@@ -309,7 +309,7 @@ class SQLSrvStatement implements IteratorAggregate, Statement
         }
 
         if (isset($args[1])) {
-            $this->defaultFetchClassCtorArgs = (array) $args[2];
+            $this->defaultFetchClassCtorArgs = (array) $args[1];
         }
 
         return true;
@@ -370,7 +370,7 @@ class SQLSrvStatement implements IteratorAggregate, Statement
 
         switch ($fetchMode) {
             case FetchMode::CUSTOM_OBJECT:
-                while (($row = $this->fetch($fetchMode, $args)) !== false) {
+                while (($row = $this->fetch($fetchMode, ...$args)) !== false) {
                     $rows[] = $row;
                 }
                 break;
