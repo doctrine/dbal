c6d8b41550573cf177a1625c5fd5eaf098fb68f5
Philipp Cordes <pc@irgendware.net>
2018-04-04 20:24:44 +0200

    Allow using PDO-only fetch modes for now
    Other drivers (including the cache) never supported fetch modes that are
    not listed in `FetchMode`. Converting between the contants is only done
    for PDO, so BC only needs to be restored here. PDO will validate the
    fetch mode, we don’t have to.

--- lib/Doctrine/DBAL/Driver/PDOStatement.php
+++ lib/Doctrine/DBAL/Driver/PDOStatement.php
@@ -22,6 +22,9 @@ namespace Doctrine\DBAL\Driver;
 use Doctrine\DBAL\FetchMode;
 use Doctrine\DBAL\ParameterType;
 use PDO;
+use const E_USER_DEPRECATED;
+use function sprintf;
+use function trigger_error;
 
 /**
  * The PDO implementation of the Statement interface.
@@ -231,7 +234,14 @@ class PDOStatement extends \PDOStatement implements Statement
         }
 
         if (! isset(self::FETCH_MODE_MAP[$fetchMode])) {
-            throw new \InvalidArgumentException('Invalid fetch mode: ' . $fetchMode);
+            // TODO: next major: throw an exception
+            @trigger_error(sprintf(
+                'Using a PDO fetch mode or their combination (%d given)' .
+                ' is deprecated and will cause an error in Doctrine 3.0',
+                $fetchMode
+            ), E_USER_DEPRECATED);
+
+            return $fetchMode;
         }
 
         return self::FETCH_MODE_MAP[$fetchMode];
