a3f8048c35db4ec14aa2fca3604a781b6b5b462a
Abin Andrews <abinandrews@gmail.com>
2018-04-07 04:49:27 +0530

    Adding support for dot character (.) when using parameters

--- lib/Doctrine/DBAL/SQLParserUtils.php
+++ lib/Doctrine/DBAL/SQLParserUtils.php
@@ -29,7 +29,7 @@ namespace Doctrine\DBAL;
 class SQLParserUtils
 {
     const POSITIONAL_TOKEN = '\?';
-    const NAMED_TOKEN      = '(?<!:):[a-zA-Z_][a-zA-Z0-9_]*';
+    const NAMED_TOKEN      = '(?<!:):[a-zA-Z_][a-zA-Z0-9_.]*';
 
     // Quote characters within string literals can be preceded by a backslash.
     const ESCAPED_SINGLE_QUOTED_TEXT = "(?:'(?:\\\\\\\\)+'|'(?:[^'\\\\]|\\\\'?|'')*')";
