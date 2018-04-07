c1e78a9302f68f93ef760fbb0bbf0eec43589290
Michael Moravec <me@majkl.me>
2018-04-07 20:28:18 +0200

    Bump version to 2.8.x-dev

--- lib/Doctrine/DBAL/Version.php
+++ lib/Doctrine/DBAL/Version.php
@@ -38,7 +38,7 @@ class Version
     /**
      * Current Doctrine Version.
      */
-    const VERSION = '2.7.0-DEV';
+    public const VERSION = '2.8.0-DEV';
 
     /**
      * Compares a Doctrine version with the current one.
