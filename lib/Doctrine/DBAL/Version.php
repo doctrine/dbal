11037b4352c008373561dc6fc836834eed80c3b5
Sergei Morozov <morozov@tut.by>
2018-04-07 11:44:18 -0700

    Preparing the 2.7.1 release

--- lib/Doctrine/DBAL/Version.php
+++ lib/Doctrine/DBAL/Version.php
@@ -38,7 +38,7 @@ class Version
     /**
      * Current Doctrine Version.
      */
-    const VERSION = '2.7.0-DEV';
+    const VERSION = '2.7.1';
 
     /**
      * Compares a Doctrine version with the current one.
