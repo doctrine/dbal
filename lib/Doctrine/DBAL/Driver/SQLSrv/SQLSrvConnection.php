2a30f670dda4af64abb11f6ab802e26e5a8f3ca4
Sergei Morozov <morozov@tut.by>
2018-04-10 23:58:28 -0700

    [DBAL-3079] Added type hints to query-related method parameters and return values

--- lib/Doctrine/DBAL/Driver/SQLSrv/SQLSrvConnection.php
+++ lib/Doctrine/DBAL/Driver/SQLSrv/SQLSrvConnection.php
@@ -3,7 +3,9 @@
 namespace Doctrine\DBAL\Driver\SQLSrv;
 
 use Doctrine\DBAL\Driver\Connection;
+use Doctrine\DBAL\Driver\ResultStatement;
 use Doctrine\DBAL\Driver\ServerInfoAwareConnection;
+use Doctrine\DBAL\Driver\Statement as DriverStatement;
 use Doctrine\DBAL\ParameterType;
 use const SQLSRV_ERR_ERRORS;
 use function is_float;
@@ -74,7 +76,7 @@ class SQLSrvConnection implements Connection, ServerInfoAwareConnection
     /**
      * {@inheritDoc}
      */
-    public function prepare($sql)
+    public function prepare(string $sql) : DriverStatement
     {
         return new SQLSrvStatement($this->conn, $sql, $this->lastInsertId);
     }
@@ -82,7 +84,7 @@ class SQLSrvConnection implements Connection, ServerInfoAwareConnection
     /**
      * {@inheritDoc}
      */
-    public function query(string $sql)
+    public function query(string $sql) : ResultStatement
     {
         $stmt = $this->prepare($sql);
         $stmt->execute();
@@ -107,7 +109,7 @@ class SQLSrvConnection implements Connection, ServerInfoAwareConnection
     /**
      * {@inheritDoc}
      */
-    public function exec($statement)
+    public function exec(string $statement) : int
     {
         $stmt = sqlsrv_query($this->conn, $statement);
 
