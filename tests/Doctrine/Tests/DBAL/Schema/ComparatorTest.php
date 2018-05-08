a99cc1a920e474a260f7b1e43ff0e89b1499ed61
maxime.perrimond <maxime.perrimond@zenport.io>
2018-05-08 19:30:53 +0900

    Fix code standard issue

--- tests/Doctrine/Tests/DBAL/Schema/ComparatorTest.php
+++ tests/Doctrine/Tests/DBAL/Schema/ComparatorTest.php
@@ -435,8 +435,8 @@ class ComparatorTest extends \PHPUnit\Framework\TestCase
         $seq1 = new Sequence('foo', 1, 1);
         $seq2 = new Sequence('foo', 1, 2);
         $seq3 = new Sequence('foo', 2, 1);
-        $seq4 = new Sequence('foo', "1", "1");
-        $seq5 = new Sequence('foo', "10", "1");
+        $seq4 = new Sequence('foo', '1', '1');
+        $seq5 = new Sequence('foo', '10', '1');
 
         $c = new Comparator();
 
