314529f55c2659a4af2a2d2c985eff2763c72c98
Sergei Morozov <morozov@tut.by>
2018-12-13 21:58:29 -0800

    Fixed test failures in case of a non-standard MySQL port

--- tests/Doctrine/Tests/DBAL/Functional/Driver/Mysqli/ConnectionTest.php
+++ tests/Doctrine/Tests/DBAL/Functional/Driver/Mysqli/ConnectionTest.php
@@ -61,6 +61,7 @@ class ConnectionTest extends DbalFunctionalTestCase
             [
                 'host' => $GLOBALS['db_host'],
                 'dbname' => $GLOBALS['db_name'],
+                'port' => $GLOBALS['db_port'],
             ],
             $GLOBALS['db_username'],
             $GLOBALS['db_password'],
