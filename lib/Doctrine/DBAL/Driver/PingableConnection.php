2ac6007314c5ea52d9711fb3c07d01925c612256
Sergei Morozov <morozov@tut.by>
2019-03-13 22:56:22 -0700

    Connection::ping() will return void

--- lib/Doctrine/DBAL/Driver/PingableConnection.php
+++ lib/Doctrine/DBAL/Driver/PingableConnection.php
@@ -11,9 +11,9 @@ interface PingableConnection
 {
     /**
      * Pings the database server to determine if the connection is still
-     * available. Return true/false based on if that was successful or not.
+     * available.
      *
-     * @return bool
+     * @throws DriverException
      */
-    public function ping();
+    public function ping() : void;
 }
