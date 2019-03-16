1ed62933f8fc5c843a95e36202aa5bcfe8069ef6
Sergei Morozov <morozov@tut.by>
2019-03-15 23:53:25 -0700

    Connection::quote() can only quote strings

--- lib/Doctrine/DBAL/Driver/PDOSqlsrv/Connection.php
+++ lib/Doctrine/DBAL/Driver/PDOSqlsrv/Connection.php
@@ -4,7 +4,6 @@ namespace Doctrine\DBAL\Driver\PDOSqlsrv;
 
 use Doctrine\DBAL\Driver\PDOConnection;
 use Doctrine\DBAL\Driver\PDOStatement;
-use Doctrine\DBAL\ParameterType;
 use function strpos;
 use function substr;
 
@@ -31,9 +30,9 @@ class Connection extends PDOConnection
     /**
      * {@inheritDoc}
      */
-    public function quote($value, $type = ParameterType::STRING)
+    public function quote(string $input) : string
     {
-        $val = parent::quote($value, $type);
+        $val = parent::quote($input);
 
         // Fix for a driver version terminating all values with null byte
         if (strpos($val, "\0") !== false) {
