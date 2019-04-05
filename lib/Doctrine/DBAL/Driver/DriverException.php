cfe3c0d1cd6254fa8f83a30c150330ecee23e167
Sergei Morozov <morozov@tut.by>
2019-04-04 18:34:27 -0700

    Reworking driver exceptions

--- lib/Doctrine/DBAL/Driver/DriverException.php
+++ lib/Doctrine/DBAL/Driver/DriverException.php
@@ -15,28 +15,9 @@ use Throwable;
 interface DriverException extends Throwable
 {
     /**
-     * Returns the driver specific error code if available.
-     *
-     * Returns null if no driver specific error code is available
-     * for the error raised by the driver.
-     *
-     * @return int|string|null
-     */
-    public function getErrorCode();
-
-    /**
-     * Returns the driver error message.
-     *
-     * @return string
-     */
-    public function getMessage();
-
-    /**
      * Returns the SQLSTATE the driver was in at the time the error occurred.
      *
      * Returns null if the driver does not provide a SQLSTATE for the error occurred.
-     *
-     * @return string|null
      */
-    public function getSQLState();
+    public function getSQLState() : ?string;
 }
