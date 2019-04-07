2d15848cbd02b61db0fa5c11f383d459b0039d2a
Sergei Morozov <morozov@tut.by>
2019-04-06 18:12:38 -0700

    Added type hints to the Column class

--- lib/Doctrine/DBAL/Schema/DB2SchemaManager.php
+++ lib/Doctrine/DBAL/Schema/DB2SchemaManager.php
@@ -47,12 +47,9 @@ class DB2SchemaManager extends AbstractSchemaManager
     {
         $tableColumn = array_change_key_case($tableColumn, CASE_LOWER);
 
-        $length    = null;
-        $fixed     = null;
-        $scale     = false;
-        $precision = false;
-
-        $default = null;
+        $length = $precision = $default = null;
+        $scale  = 0;
+        $fixed  = false;
 
         if ($tableColumn['default'] !== null && $tableColumn['default'] !== 'NULL') {
             $default = $tableColumn['default'];
@@ -68,7 +65,6 @@ class DB2SchemaManager extends AbstractSchemaManager
         switch (strtolower($tableColumn['typename'])) {
             case 'varchar':
                 $length = $tableColumn['length'];
-                $fixed  = false;
                 break;
             case 'character':
                 $length = $tableColumn['length'];
@@ -88,12 +84,10 @@ class DB2SchemaManager extends AbstractSchemaManager
         $options = [
             'length'        => $length,
             'unsigned'      => false,
-            'fixed'         => (bool) $fixed,
+            'fixed'         => $fixed,
             'default'       => $default,
             'autoincrement' => (bool) $tableColumn['autoincrement'],
-            'notnull'       => (bool) ($tableColumn['nulls'] === 'N'),
-            'scale'         => null,
-            'precision'     => null,
+            'notnull'       => $tableColumn['nulls'] === 'N',
             'comment'       => isset($tableColumn['comment']) && $tableColumn['comment'] !== ''
                 ? $tableColumn['comment']
                 : null,
