82d12fd4132b903fa691b2ce8f755553bf9e58ce
Michael Moravec <me@majkl.me>
2019-04-15 18:40:13 +0200

    SQLParserUtils::*_TOKEN constants made private

--- lib/Doctrine/DBAL/SQLParserUtils.php
+++ lib/Doctrine/DBAL/SQLParserUtils.php
@@ -28,13 +28,14 @@ use function substr;
  */
 class SQLParserUtils
 {
+    private const POSITIONAL_TOKEN = '\?';
+    private const NAMED_TOKEN      = '(?<!:):[a-zA-Z_][a-zA-Z0-9_]*';
+
     /**#@+
+     * Quote characters within string literals can be preceded by a backslash.
      *
      * @deprecated Will be removed as internal implementation details.
      */
-    public const POSITIONAL_TOKEN = '\?';
-    public const NAMED_TOKEN      = '(?<!:):[a-zA-Z_][a-zA-Z0-9_]*';
-    // Quote characters within string literals can be preceded by a backslash.
     public const ESCAPED_SINGLE_QUOTED_TEXT   = "(?:'(?:\\\\\\\\)+'|'(?:[^'\\\\]|\\\\'?|'')*')";
     public const ESCAPED_DOUBLE_QUOTED_TEXT   = '(?:"(?:\\\\\\\\)+"|"(?:[^"\\\\]|\\\\"?)*")';
     public const ESCAPED_BACKTICK_QUOTED_TEXT = '(?:`(?:\\\\\\\\)+`|`(?:[^`\\\\]|\\\\`?)*`)';
