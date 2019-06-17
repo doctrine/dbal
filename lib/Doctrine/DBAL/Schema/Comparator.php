ccde56fe060ef2362f624ecbbb23eb87819281d5
garret-gunter <garret-gunter@users.noreply.github.com>
2019-06-17 12:13:23 -0600

    Compare type class when comparing columns.

--- lib/Doctrine/DBAL/Schema/Comparator.php
+++ lib/Doctrine/DBAL/Schema/Comparator.php
@@ -12,6 +12,7 @@ use function array_shift;
 use function array_unique;
 use function assert;
 use function count;
+use function get_class;
 use function strtolower;
 
 /**
@@ -421,7 +422,11 @@ class Comparator
 
         $changedProperties = [];
 
-        foreach (['type', 'notnull', 'unsigned', 'autoincrement'] as $property) {
+        if (get_class($properties1['type']) !== get_class($properties2['type'])) {
+            $changedProperties[] = 'type';
+        }
+
+        foreach (['notnull', 'unsigned', 'autoincrement'] as $property) {
             if ($properties1[$property] === $properties2[$property]) {
                 continue;
             }
