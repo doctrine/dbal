a83e9ecb31f4f275960e973ca2ebd491e4e8d7c6
Shem Pasamba <shemgp@users.noreply.github.com>
2019-06-27 19:33:22 +0800

    Allow getCustomSchemaOption 'using' to be used
    Allow getCustomSchemaOption 'using' to be used as an additional sql to an alter column type change.  This is to allow a way of adding custom SQL that can be used to convert from one type to another.  Fixes: https://github.com/laravel/framework/issues/28963 with `syntax ->change()->using('special_price::int::numeric(12,4)')`.

--- lib/Doctrine/DBAL/Platforms/PostgreSqlPlatform.php
+++ lib/Doctrine/DBAL/Platforms/PostgreSqlPlatform.php
@@ -546,7 +546,11 @@ SQL
                 $columnDefinition['autoincrement'] = false;
 
                 // here was a server version check before, but DBAL API does not support this anymore.
-                $query = 'ALTER ' . $oldColumnName . ' TYPE ' . $type->getSQLDeclaration($columnDefinition, $this);
+                $customUsing = '';
+                if ($columnDiff->hasChanged('using')) {
+                    $customUsing = ' USING '.$column->getCustomSchemaOption('using');
+                }
+                $query = 'ALTER ' . $oldColumnName . ' TYPE ' . $type->getSQLDeclaration($columnDefinition, $this) . $customUsing;
                 $sql[] = 'ALTER TABLE ' . $diff->getName($this)->getQuotedName($this) . ' ' . $query;
             }
 
