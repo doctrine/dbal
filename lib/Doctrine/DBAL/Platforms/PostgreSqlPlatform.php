e9cfb54a922bdc379d17b7f8cd4aa0615af5a9e5
Shem Pasamba <shemgp@yahoo.com>
2019-07-03 19:58:51 +0800

    Removed use of  as requested

--- lib/Doctrine/DBAL/Platforms/PostgreSqlPlatform.php
+++ lib/Doctrine/DBAL/Platforms/PostgreSqlPlatform.php
@@ -546,11 +546,10 @@ SQL
                 $columnDefinition['autoincrement'] = false;
 
                 // here was a server version check before, but DBAL API does not support this anymore.
-                $customUsing = '';
+                $query = 'ALTER ' . $oldColumnName . ' TYPE ' . $type->getSQLDeclaration($columnDefinition, $this);
                 if ($columnDiff->hasChanged('using')) {
-                    $customUsing = ' USING ' . $column->getCustomSchemaOption('using');
+                    $query = $query . ' USING ' . $column->getCustomSchemaOption('using');
                 }
-                $query = 'ALTER ' . $oldColumnName . ' TYPE ' . $type->getSQLDeclaration($columnDefinition, $this) . $customUsing;
                 $sql[] = 'ALTER TABLE ' . $diff->getName($this)->getQuotedName($this) . ' ' . $query;
             }
 
