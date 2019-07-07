290ce2795004a4f85dc92fbb525568298513389f
Shem Pasamba <shemgp@yahoo.com>
2019-07-07 18:20:40 +0800

    Fix: 137 return error. DB2 now runs.

--- tests/travis/install-db2.sh
+++ tests/travis/install-db2.sh
@@ -6,20 +6,21 @@ echo Setting up IBM DB2
 
 sudo docker pull ibmcom/db2
 docker run \
-    -itd \
+    -d \
     --name db2 \
     --privileged=true \
     -p 50000:50000 \
     -e LICENSE=accept \
     -e DB2INST1_PASSWORD=Doctrine2018 \
     -e DBNAME=doctrine \
-    ibmcom/db2
+    ibmcom/db2 \
+    "sh -c 'tail -f /dev/null'"
 
-while ! sudo docker exec db2 su - db2inst1 -c 'which db2 2> /dev/null'; do sleep 1; done
+while ! sudo docker exec db2 su - db2inst1 -c 'which db2 2> /dev/null'; do sleep 7; done
 
-while ! sudo docker exec db2 su - db2inst1 -c 'db2start'; do sleep 1; done
+while ! docker logs db2 | grep "All databases are now active."; do sleep 7; done
 
 sudo docker exec db2 su - db2inst1 -c \
-    'db2 CREATE DB doctrine && db2 CONNECT TO doctrine && db2 CREATE USER TEMPORARY TABLESPACE doctrine_tbsp PAGESIZE 4 K'
+    'db2 CONNECT TO doctrine && db2 CREATE USER TEMPORARY TABLESPACE doctrine_tbsp PAGESIZE 4 K'
 
 echo DB2 started
