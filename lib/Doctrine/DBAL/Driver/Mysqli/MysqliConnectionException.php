532d316cfd2f6d2b2e8c549a9193806d0b6334da
Matthew J. Mucklo <mmucklo@gmail.com>
2019-08-16 23:01:13 -0700

    fix MySQL build problems with ConnectionException detection

---
+++ lib/Doctrine/DBAL/Driver/Mysqli/MysqliConnectionException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Doctrine\DBAL\Driver\Mysqli;
+
+/**
+ * Exception thrown in case the mysqli driver errors while connecting.
+ */
+class MysqliConnectionException extends MysqliException
+{
+}
---
+++ lib/Doctrine/DBAL/Driver/PDOConnectionException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Doctrine\DBAL\Driver;
+
+/**
+ * PDOConnectionException is used to distinguish connection failures from regular PDOExceptions.
+ */
+class PDOConnectionException extends PDOException
+{
+}
