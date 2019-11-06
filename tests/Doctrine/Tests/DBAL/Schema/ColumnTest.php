148aafc31216e4c4547db72f40b43d9baf778629
Grégoire Paris <postmaster@greg0ire.fr>
2019-11-06 23:09:19 +0100

    Remove phpunit-bridge specific annotations
    The phpunit-bridge has been removed in #3643, so these no longer serve
    any purpose. The tests themselves are kept since they check that there
    is no crash when using an unknown option, and do not need a phpunit
    assertion to do so.

--- tests/Doctrine/Tests/DBAL/Schema/ColumnTest.php
+++ tests/Doctrine/Tests/DBAL/Schema/ColumnTest.php
@@ -62,10 +62,6 @@ class ColumnTest extends TestCase
         self::assertEquals($expected, $this->createColumn()->toArray());
     }
 
-    /**
-     * @group legacy
-     * @expectedDeprecation The "unknown_option" column option is not supported, setting it is deprecated and will cause an error in Doctrine 3.0
-     */
     public function testSettingUnknownOptionIsStillSupported() : void
     {
         $this->expectNotToPerformAssertions();
@@ -73,10 +69,6 @@ class ColumnTest extends TestCase
         new Column('foo', $this->createMock(Type::class), ['unknown_option' => 'bar']);
     }
 
-    /**
-     * @group legacy
-     * @expectedDeprecation The "unknown_option" column option is not supported, setting it is deprecated and will cause an error in Doctrine 3.0
-     */
     public function testOptionsShouldNotBeIgnored() : void
     {
         $col1 = new Column('bar', Type::getType(Types::INTEGER), ['unknown_option' => 'bar', 'notnull' => true]);
