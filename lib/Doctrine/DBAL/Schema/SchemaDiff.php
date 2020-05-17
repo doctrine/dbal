dd6aaeae0d22a44e5c0e9082a29664d5cdde8bb9
Camille Terol <camilleterol@gmail.com>
2020-05-17 12:59:47 +0200

    Fix a condition that would not allow diffing of foreign keys for Sqlite

--- lib/Doctrine/DBAL/Schema/SchemaDiff.php
+++ lib/Doctrine/DBAL/Schema/SchemaDiff.php
@@ -115,7 +115,7 @@ class SchemaDiff
             }
         }
 
-        if ($platform->supportsForeignKeyConstraints() && $saveMode === false) {
+        if ($platform->supportsCreateDropForeignKeyConstraints() && $saveMode === false) {
             foreach ($this->orphanedForeignKeys as $orphanedForeignKey) {
                 $sql[] = $platform->getDropForeignKeySQL($orphanedForeignKey, $orphanedForeignKey->getLocalTable());
             }
