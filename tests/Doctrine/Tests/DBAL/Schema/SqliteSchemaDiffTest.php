714f4c5d36e8101b726a331e4ba6a3c6023d3adb
Camille Terol <camilleterol@gmail.com>
2020-05-21 13:55:29 +0200

    Fix the style to comply with phpcs rules

--- tests/Doctrine/Tests/DBAL/Schema/SqliteSchemaDiffTest.php
+++ tests/Doctrine/Tests/DBAL/Schema/SqliteSchemaDiffTest.php
@@ -6,9 +6,7 @@ use Doctrine\DBAL\DBALException;
 use Doctrine\DBAL\Platforms\AbstractPlatform;
 use Doctrine\DBAL\Schema\ForeignKeyConstraint;
 use Doctrine\DBAL\Schema\SchemaDiff;
-use Doctrine\DBAL\Schema\Sequence;
 use Doctrine\DBAL\Schema\Table;
-use Doctrine\DBAL\Schema\TableDiff;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
@@ -52,15 +50,15 @@ class SqliteSchemaDiffTest extends TestCase
         $platform->expects($this->exactly(1))
                 ->method('supportsSequences')
                 ->will($this->returnValue(false));
-                
+
         $platform->expects($this->exactly(0))
                 ->method('supportsForeignKeyConstraints')
                 ->will($this->returnValue(true));
-                
+
         $platform->expects($this->any())
             ->method('supportsCreateDropForeignKeyConstraints')
             ->will($this->returnValue(false));
-        
+
         $platform->expects($this->exactly(1))
             ->method('getCreateTableSql')
             ->with($this->isInstanceOf(Table::class))
@@ -71,7 +69,7 @@ class SqliteSchemaDiffTest extends TestCase
                  ->with($this->isInstanceOf(ForeignKeyConstraint::class))
                  ->will($this->throwException(
                      new DBALException('Sqlite platform does not support alter foreign key, the table must be fully recreated using getAlterTableSQL.')
-                    ));
+                 ));
 
         if ($unsafe) {
             $platform->expects($this->exactly(0))
@@ -81,8 +79,8 @@ class SqliteSchemaDiffTest extends TestCase
                          $this->isInstanceOf(Table::class)
                      )
                      ->will($this->throwException(
-                            new DBALException('Sqlite platform does not support alter foreign key, the table must be fully recreated using getAlterTableSQL.')
-                        ));
+                         new DBALException('Sqlite platform does not support alter foreign key, the table must be fully recreated using getAlterTableSQL.')
+                     ));
         }
 
         return $platform;
@@ -90,15 +88,15 @@ class SqliteSchemaDiffTest extends TestCase
 
     public function createSchemaDiff() : SchemaDiff
     {
-        $diff                              = new SchemaDiff();
+        $diff = new SchemaDiff();
 
-        $diff->newTables['foo_table']      = new Table('foo_table');
+        $diff->newTables['foo_table'] = new Table('foo_table');
         $diff->newTables['foo_table']->addColumn('foreign_id', 'integer');
         $diff->newTables['foo_table']->addForeignKeyConstraint('foreign_table', ['foreign_id'], ['id']);
 
         $fk = new ForeignKeyConstraint(['id'], 'foreign_table', ['id']);
         $fk->setLocalTable(new Table('local_table'));
-        
+
         $diff->orphanedForeignKeys[] = $fk;
 
         return $diff;
