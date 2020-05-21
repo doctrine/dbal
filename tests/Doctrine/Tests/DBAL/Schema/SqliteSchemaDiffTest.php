2341e1cd5bb7ce563b4abc99c8682240d504b4f3
Camille Terol <camilleterol@gmail.com>
2020-05-21 15:03:50 +0200

    Refactor the tests to use a dataProvider

--- tests/Doctrine/Tests/DBAL/Schema/SqliteSchemaDiffTest.php
+++ tests/Doctrine/Tests/DBAL/Schema/SqliteSchemaDiffTest.php
@@ -12,34 +12,7 @@ use PHPUnit\Framework\TestCase;
 
 class SqliteSchemaDiffTest extends TestCase
 {
-    public function testSchemaDiffToSql() : void
-    {
-        $diff     = $this->createSchemaDiff();
-        $platform = $this->createPlatform(true);
-
-        $sql = $diff->toSql($platform);
-
-        $expected = ['create_table'];
-
-        self::assertEquals($expected, $sql);
-    }
-
-    public function testSchemaDiffToSaveSql() : void
-    {
-        $diff     = $this->createSchemaDiff();
-        $platform = $this->createPlatform(false);
-
-        $sql = $diff->toSaveSql($platform);
-
-        $expected = ['create_table'];
-
-        self::assertEquals($expected, $sql);
-    }
-
-    /**
-     * @return AbstractPlatform|MockObject
-     */
-    private function createPlatform(bool $unsafe)
+    private function getGenericPlatform() 
     {
         $platform = $this->createMock(AbstractPlatform::class);
 
@@ -55,35 +28,100 @@ class SqliteSchemaDiffTest extends TestCase
                 ->method('supportsForeignKeyConstraints')
                 ->will($this->returnValue(true));
 
-        $platform->expects($this->any())
-            ->method('supportsCreateDropForeignKeyConstraints')
-            ->will($this->returnValue(false));
-
         $platform->expects($this->exactly(1))
             ->method('getCreateTableSql')
             ->with($this->isInstanceOf(Table::class))
             ->will($this->returnValue(['create_table']));
 
+        return $platform;
+    }
+
+    private function getForeignKeyConstraintsOnlyPlatform(bool $unsafe = false)
+    {
+        $platform = $this->getGenericPlatform();
+
+        $platform->expects($this->any())
+                 ->method('supportsCreateDropForeignKeyConstraints')
+                 ->will($this->returnValue(false));
+
         $platform->expects($this->exactly(0))
                  ->method('getCreateForeignKeySQL')
                  ->with($this->isInstanceOf(ForeignKeyConstraint::class))
                  ->will($this->throwException(
-                     new DBALException('Sqlite platform does not support alter foreign key, the table must be fully recreated using getAlterTableSQL.')
+                     new DBALException('This platform does not support alter foreign key, the table must be fully recreated using getAlterTableSQL.')
                  ));
 
         if ($unsafe) {
             $platform->expects($this->exactly(0))
-                     ->method('getDropForeignKeySql')
-                     ->with(
-                         $this->isInstanceOf(ForeignKeyConstraint::class),
-                         $this->isInstanceOf(Table::class)
-                     )
-                     ->will($this->throwException(
-                         new DBALException('Sqlite platform does not support alter foreign key, the table must be fully recreated using getAlterTableSQL.')
-                     ));
+                        ->method('getDropForeignKeySql')
+                        ->with(
+                            $this->isInstanceOf(ForeignKeyConstraint::class),
+                            $this->isInstanceOf(Table::class)
+                        )
+                        ->will($this->throwException(
+                            new DBALException('This platform does not support alter foreign key, the table must be fully recreated using getAlterTableSQL.')
+                        ));
         }
 
         return $platform;
+    }
+
+    private function getCreateDropForeignKeyConstraintsPlatform(bool $unsafe = false)
+    {
+        $platform = $this->getGenericPlatform();
+
+        $platform->expects($this->any())
+                 ->method('supportsCreateDropForeignKeyConstraints')
+                 ->will($this->returnValue(true));
+
+        $platform->expects($this->exactly(1))
+                 ->method('getCreateForeignKeySQL')
+                 ->with($this->isInstanceOf(ForeignKeyConstraint::class))
+                 ->will($this->returnValue('create_foreign_key'));
+
+        if ($unsafe) {
+            $platform->expects($this->exactly(1))
+                     ->method('getDropForeignKeySql')
+                     ->with(
+                        $this->isInstanceOf(ForeignKeyConstraint::class),
+                        $this->isInstanceOf(Table::class)
+                     )
+                     ->will($this->returnValue('drop_orphan_fk'));
+        }
+
+        return $platform;
+    }
+
+    public function provider(): array 
+    {
+        $diff = $this->createSchemaDiff();
+
+        return [
+            'supportsForeignKeyConstraintsOnly' => [
+                ['create_table'],
+                $diff->toSql($this->getForeignKeyConstraintsOnlyPlatform(true)),
+            ],
+            'supportsForeignKeyConstraintsOnlySaveSql' => [
+                ['create_table'],
+                $diff->toSaveSql($this->getForeignKeyConstraintsOnlyPlatform()),
+            ],
+            'supportsCreateDropForeignKeyConstraints' => [
+                ['drop_orphan_fk', 'create_table', 'create_foreign_key'],
+                $diff->toSql($this->getCreateDropForeignKeyConstraintsPlatform(true)),
+            ],
+            'supportsCreateDropForeignKeyConstraintsSaveSql' => [
+                ['create_table', 'create_foreign_key'],
+                $diff->toSaveSql($this->getCreateDropForeignKeyConstraintsPlatform()),
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider provider
+     */
+    public function testSchemaDiff($expectedActions, $sql): void
+    {
+        self::assertEquals($expectedActions, $sql);
     }
 
     public function createSchemaDiff() : SchemaDiff
