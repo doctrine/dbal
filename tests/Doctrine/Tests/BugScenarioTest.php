1c7b7cf38d2ca9faa5ac72017a7f4d2c195a7141
Youri Westerman <youri.westerman@nedac.eu>
2020-06-24 20:05:52 +0200

    Added failing test to illustrate the problem

---
+++ tests/Doctrine/Tests/BugScenarioTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Doctrine\Tests;
+
+use Doctrine\DBAL\Schema\Comparator;
+use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Types\Types;
+
+final class BugScenarioTest extends DbalFunctionalTestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $schemaManager = $this->connection->getSchemaManager();
+        $schema = $schemaManager->createSchema();
+
+        $originalTable = $schema->createTable('to_be_altered');
+        $column = $originalTable->addColumn('some_column_name', Types::TEXT);
+        $column->setNotnull(false);
+
+        $schemaManager->createTable($originalTable);
+    }
+
+    public function testInvalidQueryIsGenerated(): void
+    {
+        $schemaManager = $this->connection->getSchemaManager();
+        $schema = $schemaManager->createSchema();
+
+        $originalTable = $schema->getTable('to_be_altered');
+        $modifiedTable = clone $originalTable;
+
+        $column = $modifiedTable->getColumn('some_column_name');
+        $column->setType(Type::getType(Types::INTEGER));
+
+        $comparator = new Comparator();
+        $tableDiff = $comparator->diffTable($originalTable, $modifiedTable);
+
+        $schemaManager->alterTable($tableDiff);
+    }
+}
