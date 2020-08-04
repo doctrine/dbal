1ded32425925fc717c7f4ca73cafe577add619cd
Youri Westerman <youri.westerman@nedac.eu>
2020-08-04 15:27:54 +0200

    Added test to confirm that the charset platform option is indeed removed when changing the column type from text/string to integer

--- tests/Doctrine/Tests/DBAL/Platforms/MySqlPlatformTest.php
+++ tests/Doctrine/Tests/DBAL/Platforms/MySqlPlatformTest.php
@@ -4,7 +4,11 @@ namespace Doctrine\Tests\DBAL\Platforms;
 
 use Doctrine\DBAL\Platforms\AbstractPlatform;
 use Doctrine\DBAL\Platforms\MySqlPlatform;
+use Doctrine\DBAL\Schema\Comparator;
+use Doctrine\DBAL\Schema\Table;
 use Doctrine\DBAL\TransactionIsolationLevel;
+use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Types\Types;
 
 class MySqlPlatformTest extends AbstractMySQLPlatformTestCase
 {
@@ -19,5 +23,33 @@ class MySqlPlatformTest extends AbstractMySQLPlatformTestCase
             TransactionIsolationLevel::REPEATABLE_READ,
             $this->platform->getDefaultTransactionIsolationLevel()
         );
+    }
+
+    public function testGetAlterTableSqlRemovesCharsetPlatformOptionWhenAlteringTypeFromStringTextToInteger() : void
+    {
+        $tableA = new Table('tableA');
+        $textColumn = $tableA->addColumn('abc', Types::TEXT);
+        $textColumn->setPlatformOption('charset', 'UTF-8');
+        $stringColumn = $tableA->addColumn('def', Types::STRING);
+        $stringColumn->setPlatformOption('charset', 'SJIS');
+
+        $tableB = clone $tableA;
+        $firstColumn = $tableB->getColumn('abc');
+        $firstColumn->setType(Type::getType(Types::INTEGER));
+        $secondColumn = $tableB->getColumn('def');
+        $secondColumn->setType(Type::getType(Types::INTEGER));
+
+        $comparator = new Comparator();
+        $tableDiff = $comparator->diffTable($tableA, $tableB);
+
+        $this->platform->getAlterTableSQL($tableDiff);
+
+        self::assertGreaterThan(0, count($tableDiff->changedColumns));
+
+        foreach ($tableDiff->changedColumns as $columnDiff) {
+            $column = $columnDiff->column;
+
+            self::assertFalse($column->hasPlatformOption('charset'));
+        }
     }
 }
