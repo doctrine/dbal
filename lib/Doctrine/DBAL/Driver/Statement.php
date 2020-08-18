5b92c9ab1427b5eef137f5b9b01e0b8a3370cc04
Joe Danis <gdanis@wayfair.com>
2020-08-17 20:10:24 -0400

    Documentation on `DBAL\Driver\Statement::bindValue` is incorrect
    This change updates the documentation on the `bindValue` function. Per the implementation this function can take a string, int, or type instance.
    
    Fixes #4210

--- lib/Doctrine/DBAL/Driver/Statement.php
+++ lib/Doctrine/DBAL/Driver/Statement.php
@@ -3,6 +3,7 @@
 namespace Doctrine\DBAL\Driver;
 
 use Doctrine\DBAL\ParameterType;
+use Doctrine\DBAL\Types\Type;
 
 /**
  * Statement interface.
@@ -19,12 +20,12 @@ interface Statement extends ResultStatement
      * As mentioned above, the named parameters are not natively supported by the mysqli driver, use executeQuery(),
      * fetchAll(), fetchArray(), fetchColumn(), fetchAssoc() methods to have the named parameter emulated by doctrine.
      *
-     * @param int|string $param Parameter identifier. For a prepared statement using named placeholders,
-     *                          this will be a parameter name of the form :name. For a prepared statement
-     *                          using question mark placeholders, this will be the 1-indexed position of the parameter.
-     * @param mixed      $value The value to bind to the parameter.
-     * @param int        $type  Explicit data type for the parameter using the {@link ParameterType}
-     *                          constants.
+     * @param int|string      $param Parameter identifier. For a prepared statement using named placeholders,
+     *                               this will be a parameter name of the form :name. For a prepared statement
+     *                               using question mark placeholders, this will be the 1-indexed position of the parameter.
+     * @param mixed           $value The value to bind to the parameter.
+     * @param int|string|Type $type  Explicit data type for the parameter using the {@link ParameterType}
+     *                               constants or a reference to a {@link Type} implementation.
      *
      * @return bool TRUE on success or FALSE on failure.
      */
