5d2282389438138b91b31742f7b7f4779e1a2122
Sergei Morozov <morozov@tut.by>
2020-09-20 16:53:37 -0700

    Bump version to 2.11.1-DEV

--- lib/Doctrine/DBAL/Version.php
+++ lib/Doctrine/DBAL/Version.php
@@ -17,7 +17,7 @@ class Version
     /**
      * Current Doctrine Version.
      */
-    public const VERSION = '2.11.0';
+    public const VERSION = '2.11.1-DEV';
 
     /**
      * Compares a Doctrine version with the current one.
