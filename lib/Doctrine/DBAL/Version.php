fb5d5f2f26babf8dce217b1eb88300c22bb703a4
Sergei Morozov <morozov@tut.by>
2020-10-20 07:36:48 -0700

    Release 2.11.3

--- lib/Doctrine/DBAL/Version.php
+++ lib/Doctrine/DBAL/Version.php
@@ -17,7 +17,7 @@ class Version
     /**
      * Current Doctrine Version.
      */
-    public const VERSION = '2.11.3-DEV';
+    public const VERSION = '2.11.3';
 
     /**
      * Compares a Doctrine version with the current one.
