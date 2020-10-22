bdb7983c8f81103434547da238b53f4302b74505
Grégoire Paris <postmaster@greg0ire.fr>
2020-10-22 20:31:14 +0200

    Bump version to 2.12.1-DEV

--- lib/Doctrine/DBAL/Version.php
+++ lib/Doctrine/DBAL/Version.php
@@ -17,7 +17,7 @@ class Version
     /**
      * Current Doctrine Version.
      */
-    public const VERSION = '2.12.0';
+    public const VERSION = '2.12.1-DEV';
 
     /**
      * Compares a Doctrine version with the current one.
