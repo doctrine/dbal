f80b055324b2577748570683fcb67e0c1ab7e3bb
Michael Voříšek <mvorisek@mvorisek.cz>
2020-10-31 12:52:22 +0100

    Remove segfault comment from PDOOracle driver

--- lib/Doctrine/DBAL/Driver/PDOOracle/Driver.php
+++ lib/Doctrine/DBAL/Driver/PDOOracle/Driver.php
@@ -10,8 +10,7 @@ use PDOException;
 /**
  * PDO Oracle driver.
  *
- * WARNING: This driver gives us segfaults in our testsuites on CLOB and other
- * stuff. PDO Oracle is not maintained by Oracle or anyone in the PHP community,
+ * WARNING: PDO Oracle is not maintained by Oracle or anyone in the PHP community,
  * which leads us to the recommendation to use the "oci8" driver to connect
  * to Oracle instead.
  *
