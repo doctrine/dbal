192aa01186e709bbe7eb436a1801da5c733ebe48
Wojtek <wojciechtrusk@wp.pl>
2020-11-17 23:54:16 +0100

    #4437 MSSQL adds a redundant ORDER BY clause when with subquery with ORDER BY Test case

--- tests/Doctrine/Tests/DBAL/Platforms/SQLServer2012PlatformTest.php
+++ tests/Doctrine/Tests/DBAL/Platforms/SQLServer2012PlatformTest.php
@@ -479,4 +479,12 @@ class SQLServer2012PlatformTest extends AbstractSQLServerPlatformTestCase
         $sql         = $this->platform->modifyLimitQuery($querySql, 10);
         self::assertEquals($expectedSql, $sql);
     }
+
+    public function testModifyLimitQueryWithManyOrderBy(): void
+    {
+        $querySql    = 'SELECT col1 FROM test ORDER BY ( SELECT col2 from test ORDER BY col2 )';
+        $expectedSql = 'SELECT col1 FROM test ORDER BY ( SELECT col2 from test ORDER BY col2 ) OFFSET 0 ROWS FETCH NEXT 10 ROWS ONLY';
+        $sql         = $this->platform->modifyLimitQuery($querySql, 10);
+        self::assertEquals($expectedSql, $sql);
+    }
 }
