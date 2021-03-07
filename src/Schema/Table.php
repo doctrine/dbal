2410eab722d8fcc7a43de649d93d5078e11a7505
Sergei Morozov <morozov@tut.by>
2021-03-07 10:40:59 -0800

    Fix MissingClosureParamType issues

--- src/Schema/Table.php
+++ src/Schema/Table.php
@@ -499,7 +499,7 @@ class Table extends AbstractAsset
 
         $colNames = array_unique(array_merge($pkCols, $fkCols, array_keys($columns)));
 
-        uksort($columns, static function ($a, $b) use ($colNames): int {
+        uksort($columns, static function (string $a, string $b) use ($colNames): int {
             return array_search($a, $colNames, true) <=> array_search($b, $colNames, true);
         });
 
