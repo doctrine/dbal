2d232c177836fa84692e3f2cd5aaa96cf4dc8685
Sergei Morozov <morozov@tut.by>
2021-04-10 11:12:53 -0700

    [GH-4553] Simplify error handling in the OCI8 driver

--- src/Driver/OCI8/Result.php
+++ src/Driver/OCI8/Result.php
@@ -4,20 +4,18 @@ declare(strict_types=1);
 
 namespace Doctrine\DBAL\Driver\OCI8;
 
+use Doctrine\DBAL\Driver\Exception;
 use Doctrine\DBAL\Driver\FetchUtils;
-use Doctrine\DBAL\Driver\OCI8\Exception\PHPError;
+use Doctrine\DBAL\Driver\OCI8\Exception\Error;
 use Doctrine\DBAL\Driver\Result as ResultInterface;
 
 use function oci_cancel;
+use function oci_error;
 use function oci_fetch_all;
 use function oci_fetch_array;
 use function oci_num_fields;
 use function oci_num_rows;
-use function preg_match_all;
-use function restore_error_handler;
-use function set_error_handler;
 
-use const E_WARNING;
 use const OCI_ASSOC;
 use const OCI_FETCHSTATEMENT_BY_COLUMN;
 use const OCI_FETCHSTATEMENT_BY_ROW;
@@ -117,55 +115,18 @@ final class Result implements ResultInterface
 
     /**
      * @return mixed|false
+     *
+     * @throws Exception
      */
     private function fetch(int $mode)
     {
-        set_error_handler(
-            static function (int $errno, string $errstr, string $errfile, int $errline): bool {
-                /*
-                 * Screen PHP error messages for any error that the OCI8 extension
-                 * doesn't report via oci_error()
-                 *
-                 * @link http://www.dba-oracle.com/t_error_code_list.htm
-                 */
-                if (preg_match_all('/ORA-(\d+)/', $errstr, $matches) === 0) {
-                    // If no ORA error codes are present in error message,
-                    // skip this error and bubble this error up to
-                    // the normal error handler
-                    return false;
-                }
+        $result = oci_fetch_array($this->statement, $mode | OCI_RETURN_NULLS | OCI_RETURN_LOBS);
 
-                foreach ($matches[1] as $oraErrorCode) {
-                    switch ($oraErrorCode) {
-                        case '04061':
-                        case '04065':
-                        case '04068':
-                            throw PHPError::new(
-                                'There was an error before all rows could be fetched.',
-                                $errno,
-                                $errstr,
-                                $errfile,
-                                $errline
-                            );
-                    }
-                }
-
-                // If ORA error codes in message do not match specified blocklist
-                // ORA error codes, skip this error and bubble this error up to
-                // the normal error handler
-                return false;
-            },
-            E_WARNING
-        );
-
-        try {
-            return oci_fetch_array(
-                $this->statement,
-                $mode | OCI_RETURN_NULLS | OCI_RETURN_LOBS
-            );
-        } finally {
-            restore_error_handler();
+        if ($result === false && oci_error($this->statement) !== false) {
+            throw Error::new($this->statement);
         }
+
+        return $result;
     }
 
     /**
