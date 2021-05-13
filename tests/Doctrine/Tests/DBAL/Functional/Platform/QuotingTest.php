bd18ec4b1eaca0ece140881c2db6eb7c28f53a3d
Sergei Morozov <morozov@tut.by>
2021-05-12 17:24:55 -0700

    [GH-4643] Fix SQLServerPlatform::quoteIdentifier()

--- tests/Doctrine/Tests/DBAL/Functional/Platform/QuotingTest.php
+++ tests/Doctrine/Tests/DBAL/Functional/Platform/QuotingTest.php
@@ -2,7 +2,10 @@
 
 namespace Doctrine\Tests\DBAL\Functional\Platform;
 
+use Doctrine\DBAL\Platforms\OraclePlatform;
 use Doctrine\Tests\DbalFunctionalTestCase;
+
+use function key;
 
 class QuotingTest extends DbalFunctionalTestCase
 {
@@ -27,6 +30,43 @@ class QuotingTest extends DbalFunctionalTestCase
         return [
             'backslash' => ['\\'],
             'single-quote' => ["'"],
+        ];
+    }
+
+    /**
+     * @dataProvider identifierProvider
+     */
+    public function testQuoteIdentifier(string $identifier): void
+    {
+        $platform = $this->connection->getDatabasePlatform();
+
+        /**
+         * @link https://docs.oracle.com/cd/B19306_01/server.102/b14200/sql_elements008.htm
+         */
+        if ($platform instanceof OraclePlatform && $identifier === '"') {
+            self::markTestSkipped('Oracle does not support double quotes in identifiers');
+        }
+
+        $query = $platform->getDummySelectSQL(
+            'NULL AS ' . $platform->quoteIdentifier($identifier)
+        );
+
+        $row = $this->connection->fetchAssociative($query);
+
+        self::assertNotFalse($row);
+        self::assertSame($identifier, key($row));
+    }
+
+    /**
+     * @return iterable<string,array{0:string}>
+     */
+    public static function identifierProvider(): iterable
+    {
+        return [
+            '[' => ['['],
+            ']' => [']'],
+            '"' => ['"'],
+            '`' => ['`'],
         ];
     }
 }
