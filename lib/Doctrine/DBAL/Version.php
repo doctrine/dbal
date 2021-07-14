5c67a43dc0d7a20dfea87d1ee4139961a3983f3a
Phil Davis <phil@jankaritech.com>
2021-07-14 08:54:35 +0545

    Update VERSION constant in 2.13 series

--- lib/Doctrine/DBAL/Version.php
+++ lib/Doctrine/DBAL/Version.php
@@ -17,7 +17,7 @@ class Version
     /**
      * Current Doctrine Version.
      */
-    public const VERSION = '2.12.2-DEV';
+    public const VERSION = '2.13.2';
 
     /**
      * Compares a Doctrine version with the current one.
