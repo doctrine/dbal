d92ddb260547c2a7b650ff140f744eb6f395d8fc
Alexander M. Turek <me@derrabus.de>
2021-11-11 17:27:36 +0100

    Prepare the 2.13.5 release
    Signed-off-by: Alexander M. Turek <me@derrabus.de>

--- lib/Doctrine/DBAL/Version.php
+++ lib/Doctrine/DBAL/Version.php
@@ -17,7 +17,7 @@ class Version
     /**
      * Current Doctrine Version.
      */
-    public const VERSION = '2.13.5-dev';
+    public const VERSION = '2.13.5';
 
     /**
      * Compares a Doctrine version with the current one.
