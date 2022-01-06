f0a0491e44337240aff711b33005b3fa95d83270
Alexander M. Turek <me@derrabus.de>
2022-01-06 10:18:13 +0100

    Bump to 2.13.8-dev

--- lib/Doctrine/DBAL/Version.php
+++ lib/Doctrine/DBAL/Version.php
@@ -17,7 +17,7 @@ class Version
     /**
      * Current Doctrine Version.
      */
-    public const VERSION = '2.13.7';
+    public const VERSION = '2.13.8-dev';
 
     /**
      * Compares a Doctrine version with the current one.
