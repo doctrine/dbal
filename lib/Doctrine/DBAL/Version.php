6e22f6012b42d7932674857989fcf184e9e9b1c3
Alexander M. Turek <me@derrabus.de>
2022-01-06 10:08:04 +0100

    Release 2.13.7

--- lib/Doctrine/DBAL/Version.php
+++ lib/Doctrine/DBAL/Version.php
@@ -17,7 +17,7 @@ class Version
     /**
      * Current Doctrine Version.
      */
-    public const VERSION = '2.13.7-dev';
+    public const VERSION = '2.13.7';
 
     /**
      * Compares a Doctrine version with the current one.
