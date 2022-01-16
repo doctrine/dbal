ad58e8f519cf0c17de591a22798a5801c6a0ead0
Michael Voříšek <mvorisek@mvorisek.cz>
2022-01-16 17:02:23 +0100

    Delete run-all.sh

--- run-all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script is a small convenience wrapper for running the doctrine testsuite against a large bunch of databases.
-# Create *.phpunit.xml files and specify database connection parameters in the <php /> section.
-
-for i in *.phpunit.xml; do
-    echo "RUNNING TESTS WITH CONFIG $i"
-    vendor/bin/phpunit -c "$i" "$@"
-done
