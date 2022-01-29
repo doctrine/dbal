2c0f601b220ea94b8c2d3615ec45e0c6e6e831e8
Grégoire Paris <postmaster@greg0ire.fr>
2022-01-29 17:49:13 +0100

    Allow dynamic call to compareSchemas
    This is necessary in order to benefit from platform-aware comparison.
    As a result, tests now use platform-aware comparison, which implies
    accepting changes in how SQLitePlatform behaves.

--- tests/Platforms/SQLite/ComparatorTest.php
+++ tests/Platforms/SQLite/ComparatorTest.php
@@ -12,4 +12,9 @@ class ComparatorTest extends BaseComparatorTest
     {
         $this->comparator = new Comparator(new SqlitePlatform());
     }
+
+    public function testChangedBinaryColumn(): void
+    {
+        self::markTestSkipped('All columns are already binary in SQLite');
+    }
 }
