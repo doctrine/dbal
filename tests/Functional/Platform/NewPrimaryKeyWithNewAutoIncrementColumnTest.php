11009872126a18f7ac1fac07bb061c53440f2789
Alexander M. Turek <me@derrabus.de>
2022-07-25 19:38:08 +0200

    Use assertCount() when possible

--- tests/Functional/Platform/NewPrimaryKeyWithNewAutoIncrementColumnTest.php
+++ tests/Functional/Platform/NewPrimaryKeyWithNewAutoIncrementColumnTest.php
@@ -8,8 +8,6 @@ use Doctrine\DBAL\Schema\AbstractSchemaManager;
 use Doctrine\DBAL\Schema\Comparator;
 use Doctrine\DBAL\Tests\FunctionalTestCase;
 
-use function count;
-
 final class NewPrimaryKeyWithNewAutoIncrementColumnTest extends FunctionalTestCase
 {
     protected function setUp(): void
@@ -63,7 +61,7 @@ final class NewPrimaryKeyWithNewAutoIncrementColumnTest extends FunctionalTestCa
         self::assertTrue($validationTable->hasColumn('new_id'));
         self::assertTrue($validationTable->getColumn('new_id')->getAutoincrement());
         self::assertTrue($validationTable->hasPrimaryKey());
-        self::assertEquals(1, count($validationTable->getPrimaryKeyColumns()));
+        self::assertCount(1, $validationTable->getPrimaryKeyColumns());
         self::assertArrayHasKey('new_id', $validationTable->getPrimaryKeyColumns());
     }
 
