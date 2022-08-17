f62eeba14f2cf5b4aee9495d36e44ee94914b86b
Sergei Morozov <morozov@tut.by>
2022-08-17 11:09:58 -0700

    Fix parsing SQLite FK reference not containing whitepace

--- src/Schema/SqliteSchemaManager.php
+++ src/Schema/SqliteSchemaManager.php
@@ -626,7 +626,7 @@ SQL
                 '#
                     (?:CONSTRAINT\s+(\S+)\s+)?
                     (?:FOREIGN\s+KEY[^)]+\)\s*)?
-                    REFERENCES\s+\S+\s+(?:\([^)]+\))?
+                    REFERENCES\s+\S+\s*(?:\([^)]+\))?
                     (?:
                         [^,]*?
                         (NOT\s+DEFERRABLE|DEFERRABLE)
