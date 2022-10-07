2ceb2d349ea6c66aacb45d092f6e0c7ce7c52aeb
Sergei Morozov <morozov@tut.by>
2022-10-07 09:35:27 -0700

    Deprecate SchemaException codes

--- src/Schema/SchemaException.php
+++ src/Schema/SchemaException.php
@@ -22,17 +22,40 @@ use function sprintf;
 /** @psalm-immutable */
 class SchemaException extends Exception
 {
-    public const TABLE_DOESNT_EXIST       = 10;
-    public const TABLE_ALREADY_EXISTS     = 20;
-    public const COLUMN_DOESNT_EXIST      = 30;
-    public const COLUMN_ALREADY_EXISTS    = 40;
-    public const INDEX_DOESNT_EXIST       = 50;
-    public const INDEX_ALREADY_EXISTS     = 60;
-    public const SEQUENCE_DOENST_EXIST    = 70;
-    public const SEQUENCE_ALREADY_EXISTS  = 80;
-    public const INDEX_INVALID_NAME       = 90;
-    public const FOREIGNKEY_DOESNT_EXIST  = 100;
-    public const CONSTRAINT_DOESNT_EXIST  = 110;
+    /** @deprecated Use {@see TableDoesNotExist} instead. */
+    public const TABLE_DOESNT_EXIST = 10;
+
+    /** @deprecated Use {@see TableAlreadyExists} instead. */
+    public const TABLE_ALREADY_EXISTS = 20;
+
+    /** @deprecated Use {@see ColumnDoesNotExist} instead. */
+    public const COLUMN_DOESNT_EXIST = 30;
+
+    /** @deprecated Use {@see ColumnAlreadyExists} instead. */
+    public const COLUMN_ALREADY_EXISTS = 40;
+
+    /** @deprecated Use {@see IndexDoesNotExist} instead. */
+    public const INDEX_DOESNT_EXIST = 50;
+
+    /** @deprecated Use {@see IndexAlreadyExists} instead. */
+    public const INDEX_ALREADY_EXISTS = 60;
+
+    /** @deprecated Use {@see SequenceDoesNotExist} instead. */
+    public const SEQUENCE_DOENST_EXIST = 70;
+
+    /** @deprecated Use {@see SequenceAlreadyExists} instead. */
+    public const SEQUENCE_ALREADY_EXISTS = 80;
+
+    /** @deprecated Use {@see IndexNameInvalid} instead. */
+    public const INDEX_INVALID_NAME = 90;
+
+    /** @deprecated Use {@see ForeignKeyDoesNotExist} instead. */
+    public const FOREIGNKEY_DOESNT_EXIST = 100;
+
+    /** @deprecated Use {@see UniqueConstraintDoesNotExist} instead. */
+    public const CONSTRAINT_DOESNT_EXIST = 110;
+
+    /** @deprecated Use {@see NamespaceAlreadyExists} instead. */
     public const NAMESPACE_ALREADY_EXISTS = 120;
 
     /**
