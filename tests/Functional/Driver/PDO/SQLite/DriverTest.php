2e207c4db49006c74e3681a89bf1fc2d2b80379d
Alexander M. Turek <me@derrabus.de>
2022-10-10 21:05:13 +0200

    Deprecated the userDefinedFunctions driver option for pdo_sqlite

--- tests/Functional/Driver/PDO/SQLite/DriverTest.php
+++ tests/Functional/Driver/PDO/SQLite/DriverTest.php
@@ -2,14 +2,18 @@
 
 namespace Doctrine\DBAL\Tests\Functional\Driver\PDO\SQLite;
 
+use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Driver as DriverInterface;
 use Doctrine\DBAL\Driver\PDO\SQLite\Driver;
 use Doctrine\DBAL\Tests\Functional\Driver\AbstractDriverTest;
 use Doctrine\DBAL\Tests\TestUtil;
+use Doctrine\Deprecations\PHPUnit\VerifyDeprecations;
 
 /** @requires extension pdo_sqlite */
 class DriverTest extends AbstractDriverTest
 {
+    use VerifyDeprecations;
+
     protected function setUp(): void
     {
         parent::setUp();
@@ -29,5 +33,24 @@ class DriverTest extends AbstractDriverTest
     protected function createDriver(): DriverInterface
     {
         return new Driver();
+    }
+
+    public function testRegisterCustomFunction(): void
+    {
+        $params                                          = $this->connection->getParams();
+        $params['driverOptions']['userDefinedFunctions'] = [
+            'my_add' => ['callback' => static fn (int $a, int $b): int => $a + $b, 'numArgs' => 2],
+        ];
+
+        $connection = new Connection(
+            $params,
+            $this->connection->getDriver(),
+            $this->connection->getConfiguration(),
+            $this->connection->getEventManager(),
+        );
+
+        $this->expectDeprecationWithIdentifier('https://github.com/doctrine/dbal/pull/5742');
+
+        self::assertSame(42, (int) $connection->fetchOne('SELECT my_add(20, 22)'));
     }
 }
