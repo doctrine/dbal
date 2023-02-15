ef4b5c00f4c781e9eb5a945be0bc338a9bdefbdd
Javier Spagnoletti <phansys@gmail.com>
2023-02-14 23:17:13 -0300

    Test extension from "master" branch

--- ci/github/ext/install-pdo_ibm.sh
+++ ci/github/ext/install-pdo_ibm.sh
@@ -11,11 +11,13 @@ echo "Installing extension"
     tar -xf linuxx64_odbc_cli.tar.gz clidriver --directory .
     rm linuxx64_odbc_cli.tar.gz
 
-    wget https://pecl.php.net/get/PDO_IBM-1.5.0.tgz
+    # wget https://pecl.php.net/get/PDO_IBM-1.5.0.tgz
+    wget https://github.com/php/pecl-database-pdo_ibm/archive/refs/heads/master.tar.gz -O PDO_IBM-1.5.0.tgz
 
     tar -xf PDO_IBM-1.5.0.tgz
     rm PDO_IBM-1.5.0.tgz
-    cd PDO_IBM-1.5.0
+    # cd PDO_IBM-1.5.0
+    cd pecl-database-pdo_ibm-master
     phpize
     ./configure --with-pdo-ibm=/tmp/clidriver
     make -j "$(nproc)"
