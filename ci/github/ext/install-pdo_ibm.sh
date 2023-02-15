3f5361608e8122ecb68c652881cfe4dbd00065bb
Javier Spagnoletti <phansys@gmail.com>
2023-02-14 21:21:20 -0300

    Minor improvements at `install-pdo_ibm.sh`

--- ci/github/ext/install-pdo_ibm.sh
+++ ci/github/ext/install-pdo_ibm.sh
@@ -8,11 +8,12 @@ echo "Installing extension"
 
     wget https://public.dhe.ibm.com/ibmdl/export/pub/software/data/db2/drivers/odbc_cli/linuxx64_odbc_cli.tar.gz
 
-    tar xf linuxx64_odbc_cli.tar.gz
+    tar -xf linuxx64_odbc_cli.tar.gz clidriver --directory .
+    rm linuxx64_odbc_cli.tar.gz
 
     wget https://pecl.php.net/get/PDO_IBM-1.5.0.tgz
 
-    tar xf PDO_IBM-1.5.0.tgz
+    tar -xf PDO_IBM-1.5.0.tgz
     rm PDO_IBM-1.5.0.tgz
     cd PDO_IBM-1.5.0
     phpize
