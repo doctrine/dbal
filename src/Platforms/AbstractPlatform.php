f13dbaf4a12da09ae93a2ea21688d9b7206699d2
Alexander M. Turek <me@derrabus.de>
2023-04-02 20:27:36 +0200

    Fix reference to removed DATE_INTERVAL_UNIT_* constants

--- src/Platforms/AbstractPlatform.php
+++ src/Platforms/AbstractPlatform.php
@@ -1705,7 +1705,7 @@ abstract class AbstractPlatform
      * @param string             $operator The arithmetic operator (+ or -).
      * @param int|numeric-string $interval The interval that shall be calculated into the date.
      * @param string             $unit     The unit of the interval that shall be calculated into the date.
-     *                                     One of the DATE_INTERVAL_UNIT_* constants.
+     *                                     One of the {@see DateIntervalUnit} constants.
      *
      * @return string
      *
