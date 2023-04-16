cd876eec7f5f3c43fa4f72af8bbb48558593e8dd
Javier Spagnoletti <phansys@gmail.com>
2023-04-16 17:59:21 -0300

    Add a note about the support and the behavior of the "datetimetz" type

--- src/Types/DateTimeTzType.php
+++ src/Types/DateTimeTzType.php
@@ -11,18 +11,19 @@ use Doctrine\Deprecations\Deprecation;
 use function get_class;
 
 /**
- * DateTime type saving additional timezone information.
+ * DateTime type accepting additional information about timezone offsets.
  *
  * Caution: Databases are not necessarily experts at storing timezone related
- * data of dates. First, of all the supported vendors only PostgreSQL and Oracle
- * support storing Timezone data. But those two don't save the actual timezone
- * attached to a DateTime instance (for example "Europe/Berlin" or "America/Montreal")
- * but the current offset of them related to UTC. That means depending on daylight saving times
- * or not you may get different offsets.
+ * data of dates. First, of not all the supported vendors support storing Timezone data, and some of
+ * them only use the offset to calculate the timestamp in its default timezone (usually UTC) and persist
+ * the value without the offset information. They even don't save the actual timezone names attached
+ * to a DateTime instance (for example "Europe/Berlin" or "America/Montreal") but the current offset
+ * of them related to UTC. That means, depending on daylight saving times or not, you may get different
+ * offsets.
  *
- * This datatype makes only sense to use, if your application works with an offset, not
- * with an actual timezone that uses transitions. Otherwise your DateTime instance
- * attached with a timezone such as Europe/Berlin gets saved into the database with
+ * This datatype makes only sense to use, if your application only needs to accept the timezone offset,
+ * not the actual timezone that uses transitions. Otherwise your DateTime instance
+ * attached with a timezone such as "Europe/Berlin" gets saved into the database with
  * the offset and re-created from persistence with only the offset, not the original timezone
  * attached.
  */
