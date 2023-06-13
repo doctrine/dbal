9e1f98bbbf122dc1bcf1ff31eabef317a79dcbbb
David Maicher <mail@dmaicher.de>
2023-06-13 09:31:51 +0200

    trigger if called outside for AbstractPlatform::supportsCreateDropDatabase

--- src/Platforms/AbstractPlatform.php
+++ src/Platforms/AbstractPlatform.php
@@ -4155,7 +4155,7 @@ abstract class AbstractPlatform
      */
     public function supportsCreateDropDatabase()
     {
-        Deprecation::trigger(
+        Deprecation::triggerIfCalledFromOutside(
             'doctrine/dbal',
             'https://github.com/doctrine/dbal/pull/5513',
             '%s is deprecated.',
