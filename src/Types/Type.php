23a16fc04b1791c7383e5e306f69d96997c28c12
Adrien Foulon <6115458+Tofandel@users.noreply.github.com>
2023-06-29 11:58:35 +0200

    Update src/Types/Type.php
    Co-authored-by: Alexander M. Turek <me@derrabus.de>

--- src/Types/Type.php
+++ src/Types/Type.php
@@ -100,8 +100,8 @@ abstract class Type
     /**
      * Gets the name of this type.
      *
-     * @deprecated this method will be removed in Doctrine DBAL 4.0.
-     * You should use Type::getTypeRegistry()->lookupName($type) instead.
+     * @deprecated this method will be removed in Doctrine DBAL 4.0,
+     *             use {@see TypeRegistry::lookupName()} instead.
      *
      * @return string
      */
