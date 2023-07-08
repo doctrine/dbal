4ad68e92e0257e0e8b223a298955434208ab0196
dwgebler <davidgebler@gmail.com>
2023-07-08 19:17:35 +0100

    add pgsql driver to test check

--- tests/Functional/SQL/ParserTest.php
+++ tests/Functional/SQL/ParserTest.php
@@ -18,11 +18,10 @@ class ParserTest extends FunctionalTestCase
         self::assertTrue($this->connection->fetchOne('SELECT \'{"a":null}\'::jsonb ?? :key', ['key' => 'a']));
     }
 
-    /** test the REPLACE_PATTERNS change for ARRAY [ to ARRAY[ does not cause the mixed positional and named parameters error found prior to the fix */
     public function testParametersInArrayConstructWithWhitespace(): void
     {
-        if (! TestUtil::isDriverOneOf('pdo_pgsql')) {
-            self::markTestSkipped('This test requires the pdo_pgsql driver.');
+        if (! TestUtil::isDriverOneOf('pdo_pgsql', 'pgsql')) {
+            self::markTestSkipped('This test requires the pgsql or pdo_pgsql driver.');
         }
 
         $sql = 'SELECT * FROM (SELECT \'xyz\' AS x, \'{"foo":[1,2,3,4,5],"bar":true}\'::jsonb AS json_value) AS ' .
