191938e874f623cec152292ba539e20b9cf83a35
Sergei Morozov <morozov@tut.by>
2023-10-11 17:05:15 +0200

    Enable establishing exclusive oci8 connections

--- tests/Driver/OCI8/DriverTest.php
+++ tests/Driver/OCI8/DriverTest.php
@@ -4,10 +4,22 @@ namespace Doctrine\DBAL\Tests\Driver\OCI8;
 
 use Doctrine\DBAL\Driver as DriverInterface;
 use Doctrine\DBAL\Driver\OCI8\Driver;
+use Doctrine\DBAL\Driver\OCI8\Exception\InvalidConfiguration;
 use Doctrine\DBAL\Tests\Driver\AbstractOracleDriverTestCase;
 
+/** @requires extension oci8 */
 class DriverTest extends AbstractOracleDriverTestCase
 {
+    public function testPersistentAndExclusiveAreMutuallyExclusive(): void
+    {
+        $this->expectException(InvalidConfiguration::class);
+
+        (new Driver())->connect([
+            'persistent' => true,
+            'driverOptions' => ['exclusive' => true],
+        ]);
+    }
+
     protected function createDriver(): DriverInterface
     {
         return new Driver();
