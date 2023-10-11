f973d0b5e46ed7b1918568010322c1c7eb478644
Sylvain Beissier <sylvain.beissier@sensiolabs.com>
2023-10-11 14:47:51 +0200

    fix: truncate identity sequence name if necessary (#6184)

--- src/Platforms/PostgreSQLPlatform.php
+++ src/Platforms/PostgreSQLPlatform.php
@@ -31,8 +31,10 @@ use function is_bool;
 use function is_numeric;
 use function is_string;
 use function sprintf;
+use function strlen;
 use function strpos;
 use function strtolower;
+use function substr;
 use function trim;
 
 /**
@@ -236,7 +238,13 @@ class PostgreSQLPlatform extends AbstractPlatform
             __METHOD__,
         );
 
-        return $tableName . '_' . $columnName . '_seq';
+        $suffix = '_' . $columnName . '_seq';
+
+        if (strlen($tableName . $suffix) > $this->getMaxIdentifierLength()) {
+            $tableName = substr($tableName, 0, $this->getMaxIdentifierLength() - strlen($suffix));
+        }
+
+        return $tableName . $suffix;
     }
 
     /**
