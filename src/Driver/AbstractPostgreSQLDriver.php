46e5b94ff54c25e514a96bbec95259e4b66e593a
Allan Simon <allan.simon@supinfo.com>
2023-10-19 01:40:19 +0200

    fix #6198:  stop considering generated column definition as being its default value
    The `pg_get_expr(adbin, adrelid)` expression in Postgresql's internal table `pg_attrdef` is used to know a column definition
    
    for most column, if this returns something, it means this column's default value. So DBAL has been considering has ALWAYS meaning it contains its default.
    
    However for generated columns, it contains the value definition and not its default value, so we change the selectTableColumns' query to correctly set the 'default' column to `null` for generated columns
    
    It will help setting correctly the column's attributes which in turn will help generate correct schema diff.

--- src/Driver/AbstractPostgreSQLDriver.php
+++ src/Driver/AbstractPostgreSQLDriver.php
@@ -8,6 +8,7 @@ use Doctrine\DBAL\Driver\API\PostgreSQL;
 use Doctrine\DBAL\Exception;
 use Doctrine\DBAL\Platforms\AbstractPlatform;
 use Doctrine\DBAL\Platforms\PostgreSQL100Platform;
+use Doctrine\DBAL\Platforms\PostgreSQL120Platform;
 use Doctrine\DBAL\Platforms\PostgreSQL94Platform;
 use Doctrine\DBAL\Platforms\PostgreSQLPlatform;
 use Doctrine\DBAL\Schema\PostgreSQLSchemaManager;
@@ -39,6 +40,10 @@ abstract class AbstractPostgreSQLDriver implements VersionAwarePlatformDriver
         $minorVersion = $versionParts['minor'] ?? 0;
         $patchVersion = $versionParts['patch'] ?? 0;
         $version      = $majorVersion . '.' . $minorVersion . '.' . $patchVersion;
+
+        if (version_compare($version, '12.0', '>=')) {
+            return new PostgreSQL120Platform();
+        }
 
         if (version_compare($version, '10.0', '>=')) {
             return new PostgreSQL100Platform();
