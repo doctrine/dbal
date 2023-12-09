8a284ac726b1c640492959a94afd6b37469a0ed4
Matthew Curland <mcurland@live.com>
2023-12-08 23:44:02 -0700

    SQLite3 and OCI8, restore driver resource offsets
    Adding drivers to completed set.

--- src/Driver/SQLite3/Statement.php
+++ src/Driver/SQLite3/Statement.php
@@ -10,9 +10,14 @@ use SQLite3;
 use SQLite3Stmt;
 
 use function assert;
+use function fseek;
+use function ftell;
 use function func_num_args;
 use function is_int;
+use function is_resource;
+use function stream_get_meta_data;
 
+use const SEEK_SET;
 use const SQLITE3_BLOB;
 use const SQLITE3_INTEGER;
 use const SQLITE3_NULL;
@@ -32,6 +37,9 @@ final class Statement implements StatementInterface
 
     private SQLite3 $connection;
     private SQLite3Stmt $statement;
+
+    /** @var mixed[]|null */
+    private ?array $paramResources = null;
 
     /** @internal The statement can be only instantiated by its driver connection. */
     public function __construct(SQLite3 $connection, SQLite3Stmt $statement)
@@ -58,7 +66,12 @@ final class Statement implements StatementInterface
             );
         }
 
-        return $this->statement->bindValue($param, $value, $this->convertParamType($type));
+        $sqliteType = $this->convertParamType($type);
+        if ($sqliteType === SQLITE3_BLOB) {
+            $this->trackParamResource($value);
+        }
+
+        return $this->statement->bindValue($param, $value, $sqliteType);
     }
 
     /**
@@ -109,10 +122,15 @@ final class Statement implements StatementInterface
             }
         }
 
+        $resourceOffsets = $this->getResourceOffsets();
         try {
             $result = $this->statement->execute();
         } catch (\Exception $e) {
             throw Exception::new($e);
+        } finally {
+            if ($resourceOffsets !== null) {
+                $this->restoreResourceOffsets($resourceOffsets);
+            }
         }
 
         assert($result !== false);
@@ -132,5 +150,74 @@ final class Statement implements StatementInterface
         }
 
         return self::PARAM_TYPE_MAP[$type];
+    }
+
+    /**
+     * Track a binary parameter reference at binding time. These
+     * are cached for later analysis by the getResourceOffsets.
+     *
+     * @param mixed $resource
+     */
+    private function trackParamResource($resource): void
+    {
+        if (! is_resource($resource)) {
+            return;
+        }
+
+        $this->paramResources ??= [];
+        $this->paramResources[] = $resource;
+    }
+
+    /**
+     * Determine the offset that any resource parameters needs to be
+     * restored to after the statement is executed. Call immediately
+     * before execute (not during bindValue) to get the most accurate offset.
+     *
+     * @return int[]|null Return offsets to restore if needed. The array may be sparse.
+     */
+    private function getResourceOffsets(): ?array
+    {
+        if ($this->paramResources === null) {
+            return null;
+        }
+
+        $resourceOffsets = null;
+        foreach ($this->paramResources as $index => $resource) {
+            $position = false;
+            if (stream_get_meta_data($resource)['seekable']) {
+                $position = ftell($resource);
+            }
+
+            if ($position === false) {
+                continue;
+            }
+
+            $resourceOffsets       ??= [];
+            $resourceOffsets[$index] = $position;
+        }
+
+        if ($resourceOffsets === null) {
+            $this->paramResources = null;
+        }
+
+        return $resourceOffsets;
+    }
+
+    /**
+     * Restore resource offsets moved by PDOStatement->execute
+     *
+     * @param int[]|null $resourceOffsets The offsets returned by getResourceOffsets.
+     */
+    private function restoreResourceOffsets(?array $resourceOffsets): void
+    {
+        if ($resourceOffsets === null || $this->paramResources === null) {
+            return;
+        }
+
+        foreach ($resourceOffsets as $index => $offset) {
+            fseek($this->paramResources[$index], $offset, SEEK_SET);
+        }
+
+        $this->paramResources = null;
     }
 }
