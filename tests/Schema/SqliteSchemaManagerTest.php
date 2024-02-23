95516bae8138cdab7ad77cf779fb5dbb7ee943a0
Michael Voříšek <mvorisek@mvorisek.cz>
2024-02-24 00:26:47 +0100

    Fix DB name passing in SqliteSchemaManager::listTableForeignKeys()

--- tests/Schema/SqliteSchemaManagerTest.php
+++ tests/Schema/SqliteSchemaManagerTest.php
@@ -4,6 +4,7 @@ namespace Doctrine\DBAL\Tests\Schema;
 
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Platforms\SqlitePlatform;
+use Doctrine\DBAL\Result;
 use Doctrine\DBAL\Schema\SqliteSchemaManager;
 use PHPUnit\Framework\TestCase;
 use ReflectionMethod;
@@ -339,5 +340,34 @@ class SqliteSchemaManagerTest extends TestCase
                 )',
             ],
         ];
+    }
+
+    /**
+     * TODO move to functional test once SqliteSchemaManager::selectForeignKeyColumns can honor database/schema name
+     * https://github.com/doctrine/dbal/blob/3.8.3/src/Schema/SqliteSchemaManager.php#L740
+     */
+    public function testListTableForeignKeysDefaultDatabasePassing(): void
+    {
+        $conn = $this->createMock(Connection::class);
+
+        $manager = new class ($conn, new SqlitePlatform()) extends SqliteSchemaManager {
+            public static string $passedDatabaseName;
+
+            protected function selectForeignKeyColumns(string $databaseName, ?string $tableName = null): Result
+            {
+                self::$passedDatabaseName = $databaseName;
+
+                return parent::selectForeignKeyColumns($databaseName, $tableName);
+            }
+        };
+
+        $manager->listTableForeignKeys('t');
+        self::assertSame('main', $manager::$passedDatabaseName);
+
+        $manager->listTableForeignKeys('t', 'd');
+        self::assertSame('d', $manager::$passedDatabaseName);
+
+        $manager->listTableForeignKeys('t');
+        self::assertSame('main', $manager::$passedDatabaseName);
     }
 }
