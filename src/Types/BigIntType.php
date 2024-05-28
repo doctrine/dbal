005902422c286a68fb7ce6aded89bdd2451560a5
Michael Voříšek <mvorisek@mvorisek.cz>
2024-05-28 22:04:20 +0200

    Reimplement without "preg_replace"

--- src/Types/BigIntType.php
+++ src/Types/BigIntType.php
@@ -8,10 +8,11 @@ use Doctrine\DBAL\ParameterType;
 use Doctrine\DBAL\Platforms\AbstractPlatform;
 
 use function assert;
+use function ctype_digit;
 use function is_int;
 use function is_string;
-use function preg_replace;
 use function rtrim;
+use function str_starts_with;
 use function strpos;
 use function substr;
 
@@ -53,13 +54,27 @@ class BigIntType extends Type implements PhpIntegerMappingType
             'DBAL assumes values outside of the integer range to be returned as string by the database driver.',
         );
 
-        // workaround https://github.com/php/php-src/issues/14345
+        if (str_starts_with($value, '-') || str_starts_with($value, '+')) {
+            $hasNegativeSign = str_starts_with($value, '-');
+            $value           = substr($value, 1);
+        } else {
+            $hasNegativeSign = false;
+        }
+
+        while (substr($value, 0, 1) === '0' && ctype_digit(substr($value, 1, 1))) {
+            $value = substr($value, 1);
+        }
+
         $dotPos = strpos($value, '.');
         if ($dotPos !== false && rtrim(substr($value, $dotPos + 1), '0') === '') {
             $value = substr($value, 0, $dotPos);
         }
 
-        if (preg_replace('~^(\+|-(?=0+$))|(?<=^|^[+\-])0+(?=\d)~', '', $value) === (string) (int) $value) {
+        if ($hasNegativeSign && $value !== '0') {
+            $value = '-' . $value;
+        }
+
+        if ($value === (string) (int) $value) {
             return (int) $value;
         }
 
