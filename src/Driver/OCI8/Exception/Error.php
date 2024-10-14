f215f4b7a3bdf4db56f7cfa017609266b3babaa9
Simon Podlipsky <simon@podlipsky.net>
2024-10-14 10:28:08 +0200

    Transform Oracle's "transaction rolled back" exception and use the underlying one that DBAL supports

--- src/Driver/OCI8/Exception/Error.php
+++ src/Driver/OCI8/Exception/Error.php
@@ -7,7 +7,9 @@ namespace Doctrine\DBAL\Driver\OCI8\Exception;
 use Doctrine\DBAL\Driver\AbstractException;
 
 use function assert;
+use function explode;
 use function oci_error;
+use function str_replace;
 
 /**
  * @internal
@@ -16,12 +18,25 @@ use function oci_error;
  */
 final class Error extends AbstractException
 {
+    private const CODE_TRANSACTION_ROLLED_BACK = 2091;
+
     /** @param resource $resource */
     public static function new($resource): self
     {
         $error = oci_error($resource);
         assert($error !== false);
 
-        return new self($error['message'], null, $error['code']);
+        $code    = $error['code'];
+        $message = $error['message'];
+        if ($code === self::CODE_TRANSACTION_ROLLED_BACK) {
+            //ORA-02091: transaction rolled back
+            //ORA-00001: unique constraint (DOCTRINE.GH3423_UNIQUE) violated
+            [, $causeError] = explode("\n", $message, 2);
+
+            [$causeCode, $message] = explode(': ', $causeError, 2);
+            $code                  = (int) str_replace('ORA-', '', $causeCode);
+        }
+
+        return new self($message, null, $code);
     }
 }
