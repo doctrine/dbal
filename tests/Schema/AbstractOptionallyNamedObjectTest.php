fccc30eabf28d0df1ea850c9711c79df92b7c076
Sergei Morozov <morozov@tut.by>
2024-12-22 15:54:56 -0800

    Remove handling of non-initialized object names

--- tests/Schema/AbstractOptionallyNamedObjectTest.php
+++ tests/Schema/AbstractOptionallyNamedObjectTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Doctrine\DBAL\Tests\Schema;
 
 use Doctrine\DBAL\Schema\AbstractOptionallyNamedObject;
-use Doctrine\DBAL\Schema\Exception\NameIsNotInitialized;
 use Doctrine\DBAL\Schema\Name;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
@@ -27,19 +26,5 @@ class AbstractOptionallyNamedObjectTest extends TestCase
     {
         yield 'empty-string' => [''];
         yield 'empty-null' => [null];
-    }
-
-    public function testAccessToUninitializedName(): void
-    {
-        $object = new /** @extends AbstractOptionallyNamedObject<Name> */
-        class () extends AbstractOptionallyNamedObject {
-            /** @phpstan-ignore constructor.missingParentCall */
-            public function __construct()
-            {
-            }
-        };
-
-        $this->expectException(NameIsNotInitialized::class);
-        $object->getObjectName();
     }
 }
