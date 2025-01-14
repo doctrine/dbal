0323bd136410b8b460d59d6559ae8ab6ca024776
Damian Długosz <bigfootdd@gmail.com>
2025-01-14 23:19:58 +0100

    Add DriverTest

---
+++ tests/Driver/PDO/DBLib/DriverTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Doctrine\DBAL\Tests\Driver\PDO\DBLib;
+
+use Doctrine\DBAL\Driver\PDO\Exception\InvalidConfiguration;
+use Doctrine\DBAL\Driver\PDO\DBLib\Driver;
+use Doctrine\DBAL\Tests\Driver\AbstractSQLServerDriverTestCase;
+
+class DriverTest extends AbstractSQLServerDriverTestCase
+{
+    public function testUserIsFalse(): void
+    {
+        $this->expectException(InvalidConfiguration::class);
+        $this->expectExceptionMessage(
+            'The user configuration parameter is expected to be either a string or null, got bool.',
+        );
+        $this->driver->connect(['user' => false]);
+    }
+
+    public function testPasswordIsFalse(): void
+    {
+        $this->expectException(InvalidConfiguration::class);
+        $this->expectExceptionMessage(
+            'The password configuration parameter is expected to be either a string or null, got bool.',
+        );
+        $this->driver->connect(['password' => false]);
+    }
+
+    protected function createDriver(): Driver
+    {
+        return new Driver();
+    }
+}
