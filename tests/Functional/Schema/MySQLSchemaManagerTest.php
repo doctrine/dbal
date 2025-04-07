3345883544b8d5703a38b9a5f8eeb367a378c295
Sergei Morozov <morozov@tut.by>
2025-04-06 18:33:41 -0700

    Remove unused VerifyDeprecations

--- tests/Functional/Schema/MySQLSchemaManagerTest.php
+++ tests/Functional/Schema/MySQLSchemaManagerTest.php
@@ -20,14 +20,11 @@ use Doctrine\DBAL\Types\JsonType;
 use Doctrine\DBAL\Types\SmallFloatType;
 use Doctrine\DBAL\Types\Type;
 use Doctrine\DBAL\Types\Types;
-use Doctrine\Deprecations\PHPUnit\VerifyDeprecations;
 
 use function array_keys;
 
 class MySQLSchemaManagerTest extends SchemaManagerFunctionalTestCase
 {
-    use VerifyDeprecations;
-
     public static function setUpBeforeClass(): void
     {
         Type::addType('point', PointType::class);
