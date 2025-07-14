681684a5b9784fa6c4c92b845c597aadf5ec3934
Jonas Elfering <j.elfering@shopware.com>
2025-07-14 16:13:03 +0200

    fix: Fix length type in MySQLSchemaManager
    Fixes https://github.com/doctrine/dbal/issues/7027

--- src/Schema/MySQLSchemaManager.php
+++ src/Schema/MySQLSchemaManager.php
@@ -130,12 +130,12 @@ class MySQLSchemaManager extends AbstractSchemaManager
         switch ($dbType) {
             case 'char':
             case 'varchar':
-                $length = $tableColumn['character_maximum_length'];
+                $length = (int) $tableColumn['character_maximum_length'];
                 break;
 
             case 'binary':
             case 'varbinary':
-                $length = $tableColumn['character_octet_length'];
+                $length = (int) $tableColumn['character_octet_length'];
                 break;
 
             case 'tinytext':
