039f3c53df988b2673e40c6173e9563170e934a7
Ángel Guzmán Maeso <angel@guzmanmaeso.com>
2025-12-07 00:56:23 +0100

    feat: add infer type list in array for ArrayResult

--- src/Cache/ArrayResult.php
+++ src/Cache/ArrayResult.php
@@ -107,7 +107,7 @@ final class ArrayResult implements Result
         return [$this->columnNames, $this->rows];
     }
 
-    /** @param mixed[] $data */
+    /** @param array{0: list<string>, 1: list<list<mixed>>} $data */
     public function __unserialize(array $data): void
     {
         // Handle objects serialized with DBAL 4.1 and earlier.
