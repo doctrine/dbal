7fa039335187a4c0aa198d67bae2cdb1b7131efe
Adrian Schenk <118049326+adrian-schenk@users.noreply.github.com>
2026-06-14 14:23:22 +0200

    feat: output table and column when an InvalidColumnDeclaration occurs

--- src/Exception/InvalidColumnDeclaration.php
+++ src/Exception/InvalidColumnDeclaration.php
@@ -11,8 +11,31 @@ use function sprintf;
 
 final class InvalidColumnDeclaration extends LogicException implements Exception
 {
+
+    public string $column;
+    public string $table;
+
     public static function fromInvalidColumnType(string $columnName, InvalidColumnType $e): self
     {
-        return new self(sprintf('Column "%s" has invalid type', $columnName), 0, $e);
+        $e->setColumn($columnName);
+        return (new self('Column "%s" has invalid type', 0, $e))->setColumn($columnName);
+    }
+
+    public function setColumn(string $column): self
+    {
+        $this->column = $column;
+        return $this;
+    }
+
+    public function setTable(string $table): self
+    {
+        $this->table = $table;
+        return $this;
+    }
+
+    public function updateErrorMessage(): self
+    {
+        $this->message = sprintf('Column "%s" in table "%s" has invalid type', $this->column, $this->table);
+        return $this;
     }
 }
--- src/Exception/InvalidColumnType.php
+++ src/Exception/InvalidColumnType.php
@@ -9,4 +9,25 @@ use LogicException;
 
 abstract class InvalidColumnType extends LogicException implements Exception
 {
+  public string $tableName;
+  public string $columnName;
+
+  public function updateErrorMessage(): self
+  {
+    $this->message = sprintf('Column "%s" in table "%s": %s', $this->columnName, $this->tableName, $this->message);
+    return $this;
+  }
+
+  public function setTable(string $table): self
+  {
+      $this->tableName = $table;
+      return $this;
+  }
+
+  public function setColumn(string $column): self
+  {
+      $this->columnName = $column;
+      return $this;
+  }
+
 }
