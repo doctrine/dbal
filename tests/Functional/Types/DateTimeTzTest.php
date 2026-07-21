872237b5f1e6087ab70315133657031f3e1e8feb
Roman Ondráček <mail@romanondracek.cz>
2026-07-21 18:01:29 +0200

    Add functional test for DateTimeTz type
    Signed-off-by: Roman Ondráček <mail@romanondracek.cz>

---
+++ tests/Functional/Types/DateTimeTzTest.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Doctrine\DBAL\Tests\Functional\Types;
+
+use DateTime;
+use DateTimeInterface;
+use Doctrine\DBAL\Schema\Column;
+use Doctrine\DBAL\Schema\Table;
+use Doctrine\DBAL\Tests\FunctionalTestCase;
+use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Types\Types;
+use PHPUnit\Framework\Attributes\DataProvider;
+
+final class DateTimeTzTest extends FunctionalTestCase
+{
+    /** @return DateTimeInterface[][] */
+    public static function dataValuesProvider(): array
+    {
+        return [
+            [new DateTime('1985-09-01 10:10:10')],
+            [new DateTime('2013-10-07T04:23:19-04:00')],
+        ];
+    }
+
+    #[DataProvider('dataValuesProvider')]
+    public function testInsertAndRetrieveDateTimeTz(DateTimeInterface $expected): void
+    {
+        $table = Table::editor()
+            ->setUnquotedName('datetimetz_table')
+            ->setColumns(
+                Column::editor()
+                    ->setUnquotedName('val')
+                    ->setTypeName(Types::DATETIMETZ_MUTABLE)
+                    ->create(),
+            )
+            ->create();
+
+        $this->dropAndCreateTable($table);
+
+        $this->connection->insert(
+            'datetimetz_table',
+            ['val' => $expected],
+            ['val' => Types::DATETIMETZ_MUTABLE],
+        );
+
+        $value = Type::getType(Types::DATETIMETZ_MUTABLE)->convertToPHPValue(
+            $this->connection->fetchOne('SELECT val FROM datetimetz_table'),
+            $this->connection->getDatabasePlatform(),
+        );
+
+        self::assertInstanceOf(DateTime::class, $value);
+        self::assertEquals($expected, $value);
+    }
+}
