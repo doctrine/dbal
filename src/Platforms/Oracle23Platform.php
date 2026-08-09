27756a2ffc327c7c816aa2ed6dea45adee6da202
gertdepagter <gertdepagter@sofios.nl>
2026-08-09 17:28:44 +0200

    Fix cs issues

--- src/Platforms/Oracle23Platform.php
+++ src/Platforms/Oracle23Platform.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Doctrine\DBAL\Platforms;
 
-use Doctrine\DBAL\Connection;
-
 class Oracle23Platform extends OraclePlatform
 {
     public function supportsBulkInserts(): bool
